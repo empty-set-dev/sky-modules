e812b6124ab65581f44bc2d768e7b6aa88f6acce
Анна Севрюкова <anyasky91@yandex.ru>
2023-05-18 07:39:39 +0300

    Fixes

--- standard/Array/`Array_remove.ts
+++ standard/Array/`Array_remove.ts
@@ -1,9 +1,11 @@
-export default function Array_remove(this: Array<unknown>, v: unknown): void {
-    const i = this.indexOf(v)
+export default function Array_remove(array: Array<unknown>, v: unknown): boolean {
+    const i = array.indexOf(v)
 
     if (i === -1) {
-        return
+        return false
     }
 
-    this.splice(i, 1)
+    array.splice(i, 1)
+
+    return true
 }
