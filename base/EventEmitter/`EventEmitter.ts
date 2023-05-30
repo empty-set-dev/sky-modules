7200554db7ed8430a4fed4046bf9e7d4d2ddc6a3
Анна Севрюкова <anyasky91@yandex.ru>
2023-05-30 22:01:56 +0300

    Fixes

--- base/EventEmitter/`EventEmitter.ts
+++ base/EventEmitter/`EventEmitter.ts
@@ -1,7 +1,5 @@
 import { ObjectIndex } from 'types/defaultly'
 
-import $$events from './`EventEmitter/`EventEmitter$$events'
-
 export default class EventEmitter {
     static extend: <T extends unknown[], TT, R>(
         fn: (this: TT, ...args: T) => R
@@ -10,7 +8,7 @@ export default class EventEmitter {
     on!: (ev: ObjectIndex, fn: Function) => () => void
     emit!: (ev: string, ...args: unknown[]) => void
 
-    private [$$events]: Record<ObjectIndex, Function[]> = {}
+    private __events: Record<ObjectIndex, Function[]> = {}
 }
 
 import './`EventEmitter/`EventEmitter-extend'
