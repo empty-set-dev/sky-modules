b5572ea65dd7de93793c26259ec5beb98f76e62a
Анна Севрюкова <anyasky91@yandex.ru>
2023-06-03 16:25:54 +0300

    Fixes

--- common/PromisesPool/`PromisesPool/`PromisesPool+run.ts
+++ common/PromisesPool/`PromisesPool/`PromisesPool+run.ts
@@ -1,14 +1,15 @@
 import PromisesPool, { PromisesPoolTask } from '../`PromisesPool'
 
-PromisesPool.prototype.run = async function run(
+PromisesPool.prototype.run = async function run<T extends unknown[]>(
     this: PromisesPool,
-    task: PromisesPoolTask
+    task: PromisesPoolTask<T>,
+    ...args: T
 ): Promise<void> {
     if (this['__tasksCount'] < this['__count']) {
         let inserted = false
         const promise = new Promise<void>(resolve => {
             ++this['__tasksCount']
-            task().then(() => {
+            task(...args).then(() => {
                 --this['__tasksCount']
                 if (inserted) {
                     this['__tasks'].splice(this['__tasks'].indexOf(promise), 1)
@@ -16,9 +17,9 @@ PromisesPool.prototype.run = async function run(
                     inserted = true
                 }
                 if (this['__queue'].length) {
-                    const [task, resolve_] = this['__queue'].shift()!
+                    const [task, resolve_, args] = this['__queue'].shift()!
                     resolve_()
-                    this.run(task)
+                    this.run(task, ...args)
                 }
                 resolve()
             })
@@ -33,7 +34,7 @@ PromisesPool.prototype.run = async function run(
     } else {
         let resolve: Function
         const promise = new Promise(resolve_ => (resolve = resolve_))
-        this['__queue'].push([task, resolve!])
+        this['__queue'].push([task as PromisesPoolTask<unknown[]>, resolve!, args])
         await promise
     }
 }
---
+++ common/mysql/`mysql__select.ts
@@ -0,0 +1,16 @@
+import { Connection, Pool, RowDataPacket } from 'includes/mysql/defaultly'
+
+export default async function mysql__insert(
+    connection: Connection | Pool,
+    name: string,
+    columns: string[],
+    query?: string
+): Promise<RowDataPacket[]> {
+    return (
+        await connection.query(`
+        SELECT ${columns.map(column => `\`${column}\``)}
+        FROM \`${name}\`
+        ${query ? query : ''}
+    `)
+    )[0] as RowDataPacket[]
+}
