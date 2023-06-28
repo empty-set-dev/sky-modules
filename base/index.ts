ee72e94fe91ddee2ccf3c84187f6e9127c635763
Анна Севрюкова <anyasky91@yandex.ru>
2023-06-28 20:19:21 +0300

    Fixes

---
+++ base/index.ts
@@ -0,0 +1,2 @@
+import './EventEmitter'
+import './globalify'
---
+++ common/times/milliseconds/index.ts
@@ -0,0 +1,13 @@
+import globalify from 'base/globalify/defaultly'
+
+import * as local from './defaultly'
+
+globalify({ local })
+
+declare global {
+    const secondMs: number
+    const minuteMs: number
+    const hourMs: number
+    const dayMs: number
+    const weekMs: number
+}
