f569802feb761084dcafb19cd5cca83b72f60944
Анна Севрюкова <a.sevrukova@getic.com>
2023-09-21 19:20:17 +0300

    Fixes

--- base/globalify/`globalify.ts
+++ base/globalify/`globalify.ts
@@ -7,7 +7,7 @@ declare const global: Object
 type globalify = typeof globalify
 function globalify(module: object, target: object | string = global): void {
     if (typeof target === 'string') {
-        target = global[target] ??= {}
+        target = (global[target] as object) ??= {}
     }
 
     Object.assign(target, module)
