9df7f32ef0bc3ffa857f772f3e6fefd569e97094
Анна Севрюкова <a.sevrukova@getic.com>
2023-09-28 23:43:56 +0300

    Fixes

--- helpers/classnames/`classnames.ts
+++ helpers/classnames/`classnames.ts
@@ -1,11 +1,12 @@
-import cn from 'classnames'
+import { ArgumentArray } from 'classnames'
+import classNames from 'classnames/bind'
 
 export default function classnames(
     styles: Record<string, string>
-): (template: TemplateStringsArray, ...args: cn.ArgumentArray) => string {
+): (template: TemplateStringsArray, ...args: ArgumentArray) => string {
     let className = ''
 
-    return (template: TemplateStringsArray, ...args: cn.ArgumentArray) => {
+    return (template: TemplateStringsArray, ...args: ArgumentArray) => {
         function getClassName(str: string): string {
             if (str.indexOf('::') !== -1) {
                 className = str.split('::').join(':')
@@ -19,7 +20,8 @@ export default function classnames(
         }
 
         if (!template.raw) {
-            return cn(...template)
+            return classNames
+                .call(null, template as unknown as string, ...(args as string[]))
                 .split(' ')
                 .map(classname => getClassName(classname))
                 .join(' ')
@@ -38,7 +40,8 @@ export default function classnames(
 
         params.push(template[template.length - 1])
 
-        return cn(...params)
+        return classNames
+            .call(null, params)
             .split(' ')
             .map(classname => getClassName(classname))
             .join(' ')
