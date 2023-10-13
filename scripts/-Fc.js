f3c03adb918c798e183fc0feb4d8ee630d15f5b6
Анна Севрюкова <a.sevrukova@getic.com>
2023-10-13 16:43:49 +0300

    Fixes

--- scripts/-Fc.js
+++ scripts/-Fc.js
@@ -5,11 +5,12 @@ module.exports = function Fc({ types }) {
     return {
         visitor: {
             CallExpression(path) {
-                const isFc =
-                    path.node.callee.name === 'Fc' ||
-                    (path.node.callee.type === 'MemberExpression' &&
-                        path.node.callee.object.name === 'Fc' &&
-                        path.node.callee.property.name === 'pure')
+                const isPure =
+                    path.node.callee.type === 'MemberExpression' &&
+                    path.node.callee.object.name === 'Fc' &&
+                    path.node.callee.property.name === 'pure'
+
+                const isFc = path.node.callee.name === 'Fc' || isPure
 
                 if (
                     isFc &&
@@ -18,25 +19,23 @@ module.exports = function Fc({ types }) {
                     (path.node.arguments[0].type === 'FunctionExpression' ||
                         path.node.arguments[0].type === 'ArrowFunctionExpression')
                 ) {
-                    handleFc(types, path)
+                    handleFc(types, path, isPure)
                 }
             },
         },
     }
 }
 
-function handleFc(t, path) {
+function handleFc(t, path, isPure) {
     path.node.arguments[0].type = 'FunctionExpression'
 
-    const blockStatement = path.node.arguments[0].body
+    const blockStatement = path.get('arguments')[0].get('body')
 
     // forward new
-    if (blockStatement.body.find(node => node.type === 'ReturnStatement')) {
+    if (blockStatement.node.body.find(node => node.type === 'ReturnStatement')) {
         path.node.arguments[1] = t.identifier('true')
         return
     }
-
-    // console.log(body)
 
     const properties = []
     const methods = []
@@ -46,30 +45,109 @@ function handleFc(t, path) {
     const protectedProperties = []
     const protectedMoethod = []
 
-    function closure(block) {}
-
-    blockStatement.body.reduceRight((_, node, i) => {
+    blockStatement.node.body.reduceRight((_, node) => {
         if (node.type === 'FunctionDeclaration') {
-            blockStatement.body.splice(i, 1)
             methods.push(node)
         }
 
         if (node.type === 'VariableDeclaration') {
-            if (
-                node.declarations[0] &&
-                (node.declarations[0].init.type === 'ArrowFunctionExpression' ||
-                    node.declarations[0].init.type === 'FunctionExpression')
-            ) {
-                blockStatement.body.splice(i, 1)
-                methods.push(node)
-            } else {
-                blockStatement.body.splice(i, 1)
-                properties.push(...node.declarations)
-            }
+            node.declarations.forEach(declaration => {
+                if (
+                    declaration.init.type === 'ArrowFunctionExpression' ||
+                    declaration.init.type === 'FunctionExpression'
+                ) {
+                    methods.push(declaration)
+                } else {
+                    if (declaration.id.name) {
+                        properties.push(declaration)
+                    }
+                }
+            })
         }
     }, null)
 
-    console.log(properties)
+    blockStatement.node.body.reduceRight((_, node, i) => {
+        if (node.type === 'FunctionDeclaration') {
+            closure(blockStatement.get('body')[i].get('body'))
+            blockStatement.node.body.splice(i, 1)
+        }
+
+        if (node.type === 'VariableDeclaration') {
+            node.declarations.forEach((declaration, j) => {
+                if (
+                    declaration.init.type === 'ArrowFunctionExpression' ||
+                    declaration.init.type === 'FunctionExpression'
+                ) {
+                    closure(
+                        blockStatement.get('body')[i].get('declarations')[j].get('init').get('body')
+                    )
+                    blockStatement.node.body.splice(i, 1)
+                } else {
+                    blockStatement.node.body.splice(i, 1)
+                }
+            })
+        }
+    }, null)
+
+    closure(blockStatement)
+
+    function closure(path, variables = {}) {
+        path.traverse({
+            VariableDeclaration(path) {
+                path.node.declarations
+                    .map(declaration => declaration.id.name)
+                    .forEach(identifier => {
+                        variables[identifier] = true
+                    })
+            },
+            BlockStatement(path) {
+                path.skip()
+                closure(path, { ...variables })
+            },
+        })
+
+        path.traverse({
+            AssignmentExpression(path) {
+                if (path.node.left.type !== 'Identifier') {
+                    return
+                }
+
+                const { name } = path.node.left
+
+                if (variables[name]) {
+                    return
+                }
+
+                if (
+                    !properties.find(property => property.id.name === name) &&
+                    !methods.find(method => method.id.name === name)
+                ) {
+                    return
+                }
+
+                path.node = t.memberExpression(t.thisExpression(), t.identifier(name))
+            },
+            Identifier(path) {
+                const { name } = path.node
+
+                if (variables[name]) {
+                    return
+                }
+
+                if (
+                    !properties.find(property => property.id.name === name) &&
+                    !methods.find(method => method.id.name === name)
+                ) {
+                    return
+                }
+
+                path.replaceWith(t.memberExpression(t.thisExpression(), t.identifier(name)))
+                path.skip()
+            },
+        })
+    }
+
+    let superClass = null
 
     path.traverse({
         CallExpression(path) {
@@ -100,48 +178,54 @@ function handleFc(t, path) {
                     }
                 })
             } else if (path.node.callee.property.name === 'extends') {
-                //path.node.callee.object.name = 'this'
-            } else {
-                path.node.callee.object.name = 'this'
+                superClass = path.node.arguments[0]
             }
         },
     })
 
-    path.get('arguments')[0]
-        .get('body')
-        .pushContainer('body', t.returnStatement(magic(t, publicProperties, publicMethods)))
-}
+    if (!isPure) {
+        path.node.arguments[0].params.unshift(t.identifier('link'))
+        path.node.arguments[0].body.body.unshift(
+            t.callExpression(t.identifier('super'), [t.identifier('link')])
+        )
+    }
 
-function magic(t, properties, methods) {
-    return t.arrayExpression([
-        t.objectExpression([
-            ...properties.map(property =>
-                t.objectMethod(
-                    'get',
-                    t.identifier(property),
-                    [],
-                    t.blockStatement([t.returnStatement(t.identifier(property))])
-                )
+    path.node.arguments[0] = t.classDeclaration(
+        path.parentPath.node.id,
+        superClass ? superClass : isPure ? null : t.identifier('Entity'),
+        t.classBody([
+            t.classMethod(
+                'constructor',
+                t.identifier('constructor'),
+                path.node.arguments[0].params,
+                path.node.arguments[0].body,
+                false,
+                false,
+                false,
+                false
             ),
-            ...properties.map(property =>
-                t.objectMethod(
-                    'set',
-                    t.identifier(property),
-                    [t.identifier(`_${property}`)],
-                    t.blockStatement([
-                        t.expressionStatement(
-                            t.assignmentExpression(
-                                '=',
-                                t.identifier(property),
-                                t.identifier(`_${property}`)
-                            )
-                        ),
-                    ])
+            ...properties.map(property => {
+                return t.classProperty(
+                    t.identifier(property.id.name),
+                    property.init,
+                    null,
+                    null,
+                    false,
+                    false
                 )
-            ),
-        ]),
-        t.objectExpression(
-            methods.map(method => t.objectProperty(t.identifier(method), t.identifier(method)))
-        ),
-    ])
+            }),
+            ...methods.map(method => {
+                return t.classMethod(
+                    'method',
+                    t.identifier(method.id.name),
+                    method.init ? method.init.params : method.params,
+                    method.init ? method.init.body : method.body,
+                    false,
+                    false,
+                    false,
+                    method.init ? method.init.async : method.async
+                )
+            }),
+        ])
+    )
 }
