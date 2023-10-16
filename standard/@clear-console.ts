3da9bf6a1a1a51e5eb279d3381c05292f90a0583
Анна Севрюкова <a.sevrukova@getic.com>
2023-10-16 11:20:58 +0300

    Fixes

--- standard/@clear-console.ts
+++ standard/@clear-console.ts
@@ -1,2 +1,6 @@
-// eslint-disable-next-line no-console
-console.clear()
+import cluster from 'cluster'
+
+if (cluster.isPrimary) {
+    // eslint-disable-next-line no-console
+    console.clear()
+}
