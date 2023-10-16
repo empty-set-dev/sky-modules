d085a3afc216c385ef3029bdd888b4e775c620c5
Анна Севрюкова <a.sevrukova@getic.com>
2023-10-16 13:43:49 +0300

    Fixes

--- standard/@clear-console.ts
+++ standard/@clear-console.ts
@@ -1,9 +1,9 @@
 export {}
 
 const clusterImport = 'cluster'
-const cluster = (await import(`${clusterImport}`).catch(() => {
-    true
-})) as never as { isPrimary: boolean }
+const cluster = (await import(`${clusterImport}`).catch(() => ({
+    isPrimary: true,
+}))) as never as { isPrimary: boolean }
 
 if (cluster.isPrimary) {
     // eslint-disable-next-line no-console
