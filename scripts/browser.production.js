ec155cf25f0607d0a9ad41d0057835f684e0e3fe
Анна Севрюкова <a.sevrukova@getic.com>
2023-10-16 17:51:34 +0300

    Fixes

--- scripts/browser.production.js
+++ scripts/browser.production.js
@@ -3,8 +3,13 @@
 const child_process = require('child_process')
 const path = require('path')
 
-child_process.execSync(`${path.resolve(__dirname, '../node_modules/.bin/serve')} dist -p 80`, {
-    stdio: 'inherit',
-    stdout: 'inherit',
-    stdin: 'inherit',
-})
+const name = process.argv[2]
+
+child_process.execSync(
+    `${path.resolve(__dirname, '../node_modules/.bin/serve')} .dist/${name} -p 80`,
+    {
+        stdio: 'inherit',
+        stdout: 'inherit',
+        stdin: 'inherit',
+    }
+)
