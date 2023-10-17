595c556425e197625fb943911a01054a67b7de4b
Анна Севрюкова <a.sevrukova@getic.com>
2023-10-17 10:06:23 +0300

    Fixes

--- controllers/WasdController.ts
+++ controllers/WasdController.ts
@@ -4,6 +4,8 @@ export default class WasdController extends Effect {
 
         new EventListener(this, 'keydown', ev => {
             console.log(ev)
+            link['x'] = 0
+            link['y'] = 0
         })
     }
 }
