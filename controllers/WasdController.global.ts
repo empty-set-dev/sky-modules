7083c0282763f24d51415148d6184155fe0846f2
Анна Севрюкова <a.sevrukova@getic.com>
2023-10-21 06:35:32 +0300

    Fixes

--- controllers/WasdController.global.ts
+++ controllers/WasdController.global.ts
@@ -1,11 +1,14 @@
 import { Vector3 } from 'three/src/Three'
 import globalify from 'utilities/globalify'
+
 import * as module from './WasdController'
 
 globalify({ WasdController: module.default })
 
 declare global {
     class WasdController extends Effect {
-        constructor(link: Effects, target: Vector3)
+        force: number
+
+        constructor(link: Effects, target: Vector3, force: number)
     }
 }
