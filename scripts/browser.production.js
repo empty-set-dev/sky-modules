db5c40451cf63028071c77da39b2414f30fe1310
Анна Севрюкова <a.sevrukova@getic.com>
2023-10-22 10:51:22 +0300

    Fixes

--- scripts/browser.production.js
+++ scripts/browser.production.js
@@ -1,14 +1,21 @@
 #!/usr/bin/env node
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 const path = require('path')
+const url = require('url')
+
+const express = require('express')
+const proxy = require('express-http-proxy')
+
+const apiProxy = proxy('other_domain.com:3000/BLABLA', {
+    proxyReqPathResolver: req => url.parse(req.baseUrl).path,
+})
 
 const name = process.argv[2]
 
-const express = require('express')
 const app = express()
 
 app.use(express.static(path.join(process.cwd(), `dist/${name}`)))
-
+app.get('/api/*', apiProxy)
 app.get('*', function (req, res) {
     res.sendFile(path.join(process.cwd(), `dist/${name}`, 'index.html'))
 })
