d4b83aceab0ff541d03e68b3539449038d6bc73f
Анна Севрюкова <a.sevrukova@getic.com>
2024-04-19 14:48:28 +0300

    Fixes

--- .sky/docs/Template.tsx
+++ .sky/docs/Template.tsx
@@ -1,5 +1,4 @@
 import React, { ReactElement, useState, useMemo } from 'react'
-import MenuItem from './MenuItem';
 
 export default interface Template {
     menu: {
@@ -10,47 +9,12 @@ export default interface Template {
 export default function Template(props: Template) {
     const { menu } = props
 
-    const overviewElement: ReactElement = useMemo(() => (
-        <></>
-    ), [])
-
-    function selectSubMenuElement(
-        subMenu: typeof menu[0],
-        item: typeof subMenu.items[0],
-        i: number)
-    {
-        setBreadcrumbsElement(
-            <h1>
-                <span
-                    style={{
-                        color: 'pink',
-                        cursor: 'pointer',
-                    }}
-                    onClick={() => {
-                        setBreadcrumbsElement(
-                            <h1>{subMenu.title}</h1>
-                        )
-                        setSelectedElement(contentElements[i])
-                    }}
-                >
-                    {subMenu.title}
-                </span>
-                {' '}/{' '}
-                {item[0]}
-            </h1>
-        )
-        setSelectedElement(item[1])
-    }
-
     const contentElements = useMemo(() => (
         menu.map(subMenu => (
             <>
                 {subMenu.items.map((item, i) => (
                     <MenuItem
                         selected={false}
-                        onClick={() => {
-                            selectSubMenuElement(subMenu, item, i)
-                        }}
                     >
                         {item[0]}
                     </MenuItem>
@@ -62,7 +26,6 @@ export default function Template(props: Template) {
     const [breadcrumbsElement, setBreadcrumbsElement] = useState(
         <h1>Sky Docs</h1>
     )
-    const [selectedElement, setSelectedElement] = useState(overviewElement)
 
     const renderSubMenu = (subMenu: typeof menu[0], i) => (
         <>
