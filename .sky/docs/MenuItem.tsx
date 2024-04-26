add83f303003d8bbeba868163bb8604f8d261e88
Анна Севрюкова <a.sevrukova@getic.com>
2024-04-26 12:08:14 +0300

    Fixes

--- .sky/docs/MenuItem.tsx
+++
@@ -1,9 +0,0 @@
-import React, { PropsWithChildren } from "react";
-
-export default function MenuItem(props: { selected: boolean } & PropsWithChildren) {
-    return props.selected ? (
-        <li>{props.children}</li>
-    ) : (
-        <li><b>{props.children}</b></li>
-    )
-}
