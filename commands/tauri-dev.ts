48bc4c3faa074664b70142dfdced594fbe44fe74
Анна Севрюкова <a.sevrukova@getic.com>
2024-04-27 19:35:40 +0300

    Stage

---
+++ commands/tauri-dev.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env tsx
+import __loadSkyConfig, { __getModuleConfig } from './__loadSkyConfig'
+import __run from './__run'
+
+export namespace tauri {
+    dev()
+
+    export function dev(): void {
+        const name = process.argv[4]
+
+        if (name == null || name === '') {
+            // eslint-disable-next-line no-console
+            console.error('missing app name')
+            // eslint-disable-next-line
+            return
+        }
+
+        const skyConfig = __loadSkyConfig()
+        const skyModuleConfig = __getModuleConfig(name, skyConfig)
+
+        if (!skyModuleConfig) {
+            return
+        }
+    }
+}
