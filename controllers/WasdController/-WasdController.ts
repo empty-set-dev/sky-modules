c3c0a6df6333bc92b886fc3cd8699b96368b61fe
Анна Севрюкова <a.sevrukova@getic.com>
2024-04-29 16:01:42 +0300

    Fixes

--- controllers/WasdController/-WasdController.ts
+++ controllers/WasdController/-WasdController.ts
@@ -1,13 +1,15 @@
 import { PerspectiveCamera, Vector3 } from 'three/src/Three'
 
-export default class WasdController extends Effect {
+export interface WasdControllerOptions {
+    camera: PerspectiveCamera
+    acceleration: Vector3
+    force: number
+}
+export default class WasdController extends Link {
     force: number
 
-    constructor(
-        link: Effects,
-        options: { camera: PerspectiveCamera; acceleration: Vector3; force: number }
-    ) {
-        super(link)
+    constructor(parent: Parent, options: WasdControllerOptions) {
+        super(parent)
 
         const { acceleration, force } = options
 
@@ -15,61 +17,73 @@ export default class WasdController extends Effect {
 
         const state: number[] = [0, 0, 0, 0]
 
-        new PointerLock(this)
+        new PointerLock([this])
 
         const mouse = new Vector3()
-        new EventListener(this, 'mousemove', () => {
-            mouse.x
-        })
+        new EventListener(
+            'mousemove',
+            () => {
+                mouse.x
+            },
+            [this]
+        )
 
-        let fullscreen: Effect = null
-        new EventListener(this, 'keydown', ev => {
-            if (ev.code === 'KeyEnter') {
-                if (fullscreen) {
-                    fullscreen.dispose()
-                    fullscreen = null
+        let fullscreen: Fullscreen = null
+        new EventListener(
+            'keydown',
+            ev => {
+                if (ev.code === 'KeyEnter') {
+                    if (fullscreen) {
+                        fullscreen.destroy()
+                        fullscreen = null
+                    } else {
+                        fullscreen = new Fullscreen([this])
+                    }
                 }
 
-                fullscreen = Fullscreen(this)
-            }
+                if (ev.code === 'KeyW') {
+                    state[0] = 1
+                }
+                if (ev.code === 'KeyS') {
+                    state[1] = 1
+                }
+                if (ev.code === 'KeyD') {
+                    state[2] = 1
+                }
+                if (ev.code === 'KeyA') {
+                    state[3] = 1
+                }
 
-            if (ev.code === 'KeyW') {
-                state[0] = 1
-            }
-            if (ev.code === 'KeyS') {
-                state[1] = 1
-            }
-            if (ev.code === 'KeyD') {
-                state[2] = 1
-            }
-            if (ev.code === 'KeyA') {
-                state[3] = 1
-            }
+                acceleration
+                    .set(state[2] - state[3], state[0] - state[1], 0)
+                    .normalize()
+                    .multiplyScalar(this.force)
+            },
+            [this]
+        )
 
-            acceleration
-                .set(state[2] - state[3], state[0] - state[1], 0)
-                .normalize()
-                .multiplyScalar(this.force)
-        })
+        new EventListener(
+            'keyup',
+            ev => {
+                if (ev.code === 'KeyW') {
+                    state[0] = 0
+                }
+                if (ev.code === 'KeyS') {
+                    state[1] = 0
+                }
+                if (ev.code === 'KeyD') {
+                    state[2] = 0
+                }
+                if (ev.code === 'KeyA') {
+                    state[3] = 0
+                }
 
-        new EventListener(this, 'keyup', ev => {
-            if (ev.code === 'KeyW') {
-                state[0] = 0
-            }
-            if (ev.code === 'KeyS') {
-                state[1] = 0
-            }
-            if (ev.code === 'KeyD') {
-                state[2] = 0
-            }
-            if (ev.code === 'KeyA') {
-                state[3] = 0
-            }
-
-            acceleration
-                .set(state[2] - state[3], state[0] - state[1], 0)
-                .normalize()
-                .multiplyScalar(this.force)
-        })
+                acceleration
+                    .set(state[2] - state[3], state[0] - state[1], 0)
+                    .normalize()
+                    .multiplyScalar(this.force)
+            },
+            [this]
+        )
     }
 }
