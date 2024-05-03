40d25aacece54d856abcb8fb83c025f3397f780a
Анна Севрюкова <a.sevrukova@getic.com>
2024-05-03 13:20:43 +0300

    Fixes

--- commands/init-package.ts
+++ commands/init-package.ts
@@ -2,14 +2,67 @@
 import fs from 'fs'
 
 import { b, e, purple } from './__coloredConsole'
+import __loadSkyConfig from './__loadSkyConfig'
 
 export namespace init {
+    const browserCommands = ['dev', 'build', 'start']
+    const nodeCommands = ['dev', 'build', 'start']
+    const tauriCommands = ['init', 'dev', 'build', 'start']
+
     package_()
 
     export function package_(): void {
+        const skyConfig = __loadSkyConfig()
+
         const packageJson = fs.existsSync('package.json')
             ? JSON.parse(fs.readFileSync('package.json', 'utf-8'))
             : {}
+
+        packageJson.scripts = { ...skyConfig.scripts }
+        skyConfig.apps.forEach(app => {
+            if (app.platforms.includes('native') || app.platforms.length > 1) {
+                if (app.platforms.includes('native') || app.platforms.includes('browser')) {
+                    browserCommands.forEach(
+                        command =>
+                            (packageJson.scripts[
+                                `${app.name}:browser:${command}`
+                            ] = `sky browser ${command}`)
+                    )
+                }
+                if (app.platforms.includes('native')) {
+                    tauriCommands.forEach(
+                        command =>
+                            (packageJson.scripts[
+                                `${app.name}:tauri:${command}`
+                            ] = `sky tauri ${command}`)
+                    )
+                }
+                if (app.platforms.includes('node')) {
+                    nodeCommands.forEach(
+                        command =>
+                            (packageJson.scripts[
+                                `${app.name}:node:${command}`
+                            ] = `sky node ${command}`)
+                    )
+                }
+            } else {
+                if (app.platforms.includes('native') || app.platforms.includes('browser')) {
+                    browserCommands.forEach(
+                        command =>
+                            (packageJson.scripts[
+                                `${app.name}:${command}`
+                            ] = `sky browser ${command}`)
+                    )
+                }
+                if (app.platforms.includes('node')) {
+                    nodeCommands.forEach(
+                        command =>
+                            (packageJson.scripts[`${app.name}:${command}`] = `sky node ${command}`)
+                    )
+                }
+            }
+        })
+        packageJson.scripts['format'] = 'sky format'
 
         process.stdout.write(`${b}${purple}Rewrite package.json${e}`)
         fs.writeFileSync('package.json', JSON.stringify(packageJson, null, '    '), 'utf-8')
