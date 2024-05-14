14376233bd22f4ad2313d9667da468c22cb372b6
Анна Севрюкова <anyasky91@yandex.ru>
2024-05-14 09:46:15 +0300

    Fixes

---
+++ commands/tauri-build.ts
@@ -0,0 +1,37 @@
+#!/usr/bin/env -S npx tsx
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+import __loadSkyConfig, { __getModuleConfig } from './__loadSkyConfig'
+import __run from './__run'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const sdkNodeModulesPath = path.resolve(__dirname, '../node_modules')
+
+export namespace tauri {
+    build()
+
+    export function build(): void {
+        const name = process.argv[4]
+
+        if (name == null || name === '') {
+            // eslint-disable-next-line no-console
+            console.error('missing app name')
+            // eslint-disable-next-line
+            return
+        }
+
+        const skyConfig = __loadSkyConfig()
+        const skyModuleConfig = __getModuleConfig(name, skyConfig)
+
+        if (!skyModuleConfig) {
+            return
+        }
+
+        __run(path.join(sdkNodeModulesPath, '.bin/tauri') + ' build', {
+            cwd: path.resolve(skyModuleConfig['tauri-assets']),
+        })
+    }
+}
