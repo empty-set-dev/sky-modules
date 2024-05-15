f0d1fa8ada7d10fa9de01a7e5c694533e3d9253b
Анна Севрюкова <anyasky91@yandex.ru>
2024-05-15 14:00:25 +0300

    Fixes

---
+++ tests/browser/pages/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const cx = classnames(`HomePage`, () => import('./Home.module.scss'))
+
+export default function HomePage(): ReactNode {
+    return <div className={cx`b:HomePage`}>Hello, World!</div>
+}
