521f326f83261798849274fd4ae719d1a2968584
Анна Севрюкова <anyasky91@yandex.ru>
2024-05-16 10:01:02 +0300

    Fixes

--- helpers/EventEmitter/-EventEmitter+emit.ts
+++ helpers/EventEmitter/-EventEmitter+emit.ts
@@ -1,6 +1,12 @@
 import EventEmitter from './-EventEmitter'
 
-export default function emit(this: EventEmitter, ev: Object.Index, ...args: unknown[]): void {
+export default function emit(
+    this: EventEmitter,
+    ev: Object.Index,
+    ...args: unknown[]
+): EventEmitter {
     const eventsList = this['__events'][ev]
     eventsList && eventsList.forEach(cb => cb(...args))
+
+    return this
 }
