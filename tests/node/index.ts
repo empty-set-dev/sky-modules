77f7cda74f8e2bd3e21069e82896b2f837e06aeb
Анна Севрюкова <anyasky91@yandex.ru>
2024-05-16 10:38:40 +0300

    Fixes

--- tests/node/index.ts
+++ tests/node/index.ts
@@ -1,1 +1,9 @@
+import '@node/@clear-console'
+
+// eslint-disable-next-line no-console
 console.log('Hello, World!')
+
+setTimeout(() => {
+    // eslint-disable-next-line no-console
+    console.log('Timeout')
+}, 1000)
