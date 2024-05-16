f3cdef7f123e2d14cf04a2142e7a98d880ea5e05
Анна Севрюкова <anyasky91@yandex.ru>
2024-05-16 10:47:18 +0300

    Fixes

--- commands/init-package.ts
+++ commands/init-package.ts
@@ -29,61 +29,69 @@ export namespace init {
             ],
         }
 
-        packageJson.scripts = { ...skyConfig.scripts }
-        skyConfig.apps.forEach(app => {
-            if (app.platforms.length > 1) {
-                if (app.platforms.includes('native')) {
-                    tauriCommands.forEach(
-                        command =>
-                            (packageJson.scripts[
-                                `${app.name}:tauri:${command}`
-                            ] = `sky tauri ${command} ${app.name}`)
-                    )
+        if (skyConfig.scripts === false) {
+            delete packageJson.scripts
+        } else {
+            packageJson.scripts = { ...(skyConfig.scripts as Record<string, string>) }
+            skyConfig.apps.forEach(app => {
+                if (app.scripts === false) {
+                    return
                 }
-                if (app.platforms.includes('browser')) {
-                    browserCommands.forEach(
-                        command =>
-                            (packageJson.scripts[
-                                `${app.name}:browser:${command}`
-                            ] = `sky browser ${command} ${app.name}`)
-                    )
+
+                if (app.platforms.length > 1) {
+                    if (app.platforms.includes('native')) {
+                        tauriCommands.forEach(
+                            command =>
+                                (packageJson.scripts[
+                                    `${app.name}:tauri:${command}`
+                                ] = `sky tauri ${command} ${app.name}`)
+                        )
+                    }
+                    if (app.platforms.includes('browser')) {
+                        browserCommands.forEach(
+                            command =>
+                                (packageJson.scripts[
+                                    `${app.name}:browser:${command}`
+                                ] = `sky browser ${command} ${app.name}`)
+                        )
+                    }
+                    if (app.platforms.includes('node')) {
+                        nodeCommands.forEach(
+                            command =>
+                                (packageJson.scripts[
+                                    `${app.name}:node:${command}`
+                                ] = `sky node ${command} ${app.name}`)
+                        )
+                    }
+                } else {
+                    if (app.platforms.includes('native')) {
+                        tauriCommands.forEach(
+                            command =>
+                                (packageJson.scripts[
+                                    `${app.name}:${command}`
+                                ] = `sky browser ${command} ${app.name}`)
+                        )
+                    }
+                    if (app.platforms.includes('browser')) {
+                        browserCommands.forEach(
+                            command =>
+                                (packageJson.scripts[
+                                    `${app.name}:${command}`
+                                ] = `sky browser ${command} ${app.name}`)
+                        )
+                    }
+                    if (app.platforms.includes('node')) {
+                        nodeCommands.forEach(
+                            command =>
+                                (packageJson.scripts[
+                                    `${app.name}:${command}`
+                                ] = `sky node ${command} ${app.name}`)
+                        )
+                    }
                 }
-                if (app.platforms.includes('node')) {
-                    nodeCommands.forEach(
-                        command =>
-                            (packageJson.scripts[
-                                `${app.name}:node:${command}`
-                            ] = `sky node ${command} ${app.name}`)
-                    )
-                }
-            } else {
-                if (app.platforms.includes('native')) {
-                    tauriCommands.forEach(
-                        command =>
-                            (packageJson.scripts[
-                                `${app.name}:${command}`
-                            ] = `sky browser ${command} ${app.name}`)
-                    )
-                }
-                if (app.platforms.includes('browser')) {
-                    browserCommands.forEach(
-                        command =>
-                            (packageJson.scripts[
-                                `${app.name}:${command}`
-                            ] = `sky browser ${command} ${app.name}`)
-                    )
-                }
-                if (app.platforms.includes('node')) {
-                    nodeCommands.forEach(
-                        command =>
-                            (packageJson.scripts[
-                                `${app.name}:${command}`
-                            ] = `sky node ${command} ${app.name}`)
-                    )
-                }
-            }
-        })
-        packageJson.scripts['format'] = 'sky format'
+            })
+            packageJson.scripts['format'] = 'sky format'
+        }
 
         process.stdout.write(`${b}${purple}Rewrite package.json${e}`)
         fs.writeFileSync('package.json', JSON.stringify(packageJson, null, '    '), 'utf-8')
