6ebac0991444b12dca6381170bfbde52e3502f58
Анна Севрюкова <anyasky91@yandex.ru>
2024-06-16 14:40:23 +0300

    Done

--- tests/universal/index.tsx
+++ tests/universal/index.tsx
@@ -1,3 +1,18 @@
+import { View, Text } from 'react-native'
+
 export default function App(): ReactNode {
-    return <div>Hello, World!</div>
+    return (
+        <View
+            style={{
+                width: '100%',
+                height: '100%',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+        >
+            <Text>
+                Universal React with <b>Vite</b>, <b>Tauri</b> and <b>Expo</b>
+            </Text>
+        </View>
+    )
 }
