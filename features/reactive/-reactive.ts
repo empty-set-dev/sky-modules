11b5bc436a442f5c5e7ca4a08ab00ae08700c70a
Анна Севрюкова <anyasky91@yandex.ru>
2024-06-17 10:25:13 +0300

    Fixes

--- features/reactive/-reactive.ts
+++
@@ -1,9 +0,0 @@
-function reactive<T>(reaction: () => T): T
-function reactive<T>(constructor: any, a2, a3?): void
-function reactive<T>(...args: unknown[]): unknown {
-    if (typeof args[0] === 'function') {
-        return args[0] as unknown as T
-    }
-}
-
-export default reactive
