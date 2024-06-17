65b70cb3746b2b66ecdf9d72ba412b29225f2bcb
Анна Севрюкова <anyasky91@yandex.ru>
2024-06-17 10:39:36 +0300

    Fixes

--- controllers/ThirdPersonCameraController/-ThirdPersonCameraController.ts
+++ controllers/ThirdPersonCameraController/-ThirdPersonCameraController.ts
@@ -4,10 +4,10 @@ import { PerspectiveCamera } from 'three/src/cameras/PerspectiveCamera'
 export interface ThirdPersonCameraControllerOptions {
     camera: PerspectiveCamera
     getTarget: () => Vector3
-    distance: number
-    z?: number
-    minAngle?: number
-    maxAngle?: number
+    distance: () => number
+    z?: () => number
+    minAngle?: () => number
+    maxAngle?: () => number
     onUpdate?: () => void
 }
 @effect
@@ -15,10 +15,10 @@ export default class ThirdPersonCameraController extends Effect {
     camera: PerspectiveCamera
     getTarget: () => Vector3
     readonly angles = [0, 0]
-    minAngle = -Math.PI / 2 + 0.001
-    maxAngle = Math.PI / 2 - 0.001
-    distance: number
-    z: number
+    minAngle = (): number => -Math.PI / 2 + 0.001
+    maxAngle = (): number => Math.PI / 2 - 0.001
+    distance: () => number
+    z: () => number
 
     constructor(deps: EffectDeps, options: ThirdPersonCameraControllerOptions) {
         super(deps)
@@ -26,51 +26,19 @@ export default class ThirdPersonCameraController extends Effect {
         const { onUpdate } = options
         this.camera = options.camera
         this.getTarget = options.getTarget
-        this.minAngle = options.minAngle ?? -Math.PI / 2 + 0.001
-        this.maxAngle = options.maxAngle ?? Math.PI / 2 - 0.001
-        this.distance = options.distance ?? 10
-        this.z = options.z ?? 0
+        this.minAngle = options.minAngle ?? ((): number => -Math.PI / 2 + 0.001)
+        this.maxAngle = options.maxAngle ?? ((): number => Math.PI / 2 - 0.001)
+        this.distance = options.distance ?? ((): number => 10)
+        this.z = options.z ?? ((): number => 0)
 
-        new WindowEventListener(
-            'mousedown',
-            () => {
-                if (this.__pointerLock) {
-                    return
-                }
-
-                this.__pointerLock = new PointerLock(this)
-                new DocumentEventListener(
-                    'pointerlockchange',
-                    () => {
-                        new DocumentEventListener(
-                            'pointerlockchange',
-                            () => {
-                                new Timeout(
-                                    () => {
-                                        this.__pointerLock.destroy()
-                                        delete this.__pointerLock
-                                    },
-                                    2000,
-                                    [this, this.__pointerLock]
-                                )
-                            },
-                            [this, this.__pointerLock],
-                            { once: true }
-                        )
-                    },
-                    [this, this.__pointerLock],
-                    { once: true }
-                )
-            },
-            [this]
-        )
+        new SmartPointerLock(this)
 
         new WindowEventListener(
             'mousemove',
             ev => {
                 this.angles[0] += ev.movementX * 0.01
                 this.angles[1] += ev.movementY * 0.01
-                this.angles[1] = Math.minmax(this.angles[1], this.minAngle, this.maxAngle)
+                this.angles[1] = Math.minmax(this.angles[1], this.minAngle(), this.maxAngle())
                 onUpdate && onUpdate()
             },
             [this]
@@ -78,17 +46,21 @@ export default class ThirdPersonCameraController extends Effect {
     }
 
     afterAnimationFrame(): void {
+        if (!this.__smartPointerLock.isLocked) {
+            return
+        }
+
         const { camera, distance } = this
         const target = this.getTarget()
 
         camera.position.x =
-            target.x + Math.cos(this.angles[0]) * Math.cos(this.angles[1]) * distance
+            target.x + Math.cos(this.angles[0]) * Math.cos(this.angles[1]) * distance()
         camera.position.y =
-            target.y - Math.sin(this.angles[0]) * Math.cos(this.angles[1]) * distance
-        camera.position.z = target.z + this.z + Math.sin(this.angles[1]) * distance
-        camera.lookAt(new Vector3(target.x, target.y, target.z + this.z))
-        this.angles[1] = Math.minmax(this.angles[1], this.minAngle, this.maxAngle)
+            target.y - Math.sin(this.angles[0]) * Math.cos(this.angles[1]) * distance()
+        camera.position.z = target.z + this.z() + Math.sin(this.angles[1]) * distance()
+        camera.lookAt(new Vector3(target.x, target.y, target.z + this.z()))
+        this.angles[1] = Math.minmax(this.angles[1], this.minAngle(), this.maxAngle())
     }
 
-    private __pointerLock: PointerLock
+    __smartPointerLock = new SmartPointerLock(this)
 }
