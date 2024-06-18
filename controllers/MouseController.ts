3d3d9e0f3fcb42186fc7e20807c8c3640990c08e
Анна Севрюкова <anyasky91@yandex.ru>
2024-06-18 08:17:56 +0300

    Fixes

--- controllers/MouseController.ts
+++ controllers/MouseController.ts
@@ -1,8 +1,24 @@
 import Vector2 from 'sky/math/Vector2'
+import Vector3 from 'sky/math/Vector3'
 import SkyRenderer from 'sky/renderers/SkyRenderer'
+import { Camera } from 'three/src/cameras/Camera'
 
+export interface MouseControllerOptions {
+    onUpdate?: () => void
+}
 export default class MouseController extends Effect {
     mouse = new Vector2()
+    onUpdate?: () => void
+
+    constructor(deps: EffectDeps, options?: MouseControllerOptions) {
+        super(deps)
+
+        if (options) {
+            const { onUpdate } = options
+
+            this.onUpdate = onUpdate
+        }
+    }
 
     onSkyRendererContext(): void {
         new WindowEventListener(
@@ -22,10 +38,21 @@ export default class MouseController extends Effect {
         )
     }
 
+    cameraProjectionXY(options: { camera: Camera; z: number }): Vector2 {
+        const { camera, z } = options
+        const vec = new Vector3(this.mouse.x, this.mouse.y, z)
+        vec.unproject(camera)
+        vec.sub(camera.position).normalize()
+        const distance = -camera.position.z / vec.z
+        const position = new Vector3().copy(camera.position).add(vec.multiplyScalar(distance))
+        return new Vector2().copy(position)
+    }
+
     __updateMouse(): void {
         const renderer = this.context(SkyRenderer)
         const [w, h] = renderer.size()
         this.mouse.set((this.__mouse.x / w) * 2 - 1, -(this.__mouse.y / h) * 2 + 1)
+        this.onUpdate && this.onUpdate()
     }
 
     private __mouse: Vector2
