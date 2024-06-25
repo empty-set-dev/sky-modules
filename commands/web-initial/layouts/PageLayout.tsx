219c58accac5b1e70b12ea5d36fd48e3c9ab268a
Анна Севрюкова <anyasky91@yandex.ru>
2024-06-25 14:38:25 +0300

    Fixes

--- commands/web-initial/layouts/PageLayout.tsx
+++
@@ -1,8 +0,0 @@
-import '@/scss/index.scss'
-import './PageLayout.scss'
-
-import React from 'react'
-
-export function PageLayout({ children }: { children: React.ReactNode }): ReactNode {
-    return children
-}
--- tests/web/layouts/PageLayout.tsx
+++ tests/web/layouts/PageLayout.tsx
@@ -1,8 +1,85 @@
-import '@/scss/index.scss'
 import './PageLayout.scss'
 
 import React from 'react'
 
+import logoUrl from '/favicon.svg'
+
+import { Link } from 'sky/components/Link'
+
 export function PageLayout({ children }: { children: React.ReactNode }): ReactNode {
-    return children
+    return (
+        <Layout>
+            <Sidebar>
+                <Logo />
+                <Link href="/">Welcome</Link>
+                <Link href="/about">About</Link>
+                <Link href="/star-wars">Data Fetching</Link>
+            </Sidebar>
+            <Content>{children}</Content>
+        </Layout>
+    )
+}
+
+function Layout({ children }: { children: ReactNode }): ReactNode {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                maxWidth: 900,
+                margin: 'auto',
+            }}
+        >
+            {children}
+        </div>
+    )
+}
+
+function Sidebar({ children }: { children: ReactNode }): ReactNode {
+    return (
+        <div
+            id="sidebar"
+            style={{
+                padding: 20,
+                flexShrink: 0,
+                display: 'flex',
+                flexDirection: 'column',
+                lineHeight: '1.8em',
+                borderRight: '2px solid #eee',
+            }}
+        >
+            {children}
+        </div>
+    )
+}
+
+function Content({ children }: { children: React.ReactNode }): ReactNode {
+    return (
+        <div id="page-container">
+            <div
+                id="page-content"
+                style={{
+                    padding: 20,
+                    paddingBottom: 50,
+                    minHeight: '100vh',
+                }}
+            >
+                {children}
+            </div>
+        </div>
+    )
+}
+
+function Logo(): ReactNode {
+    return (
+        <div
+            style={{
+                marginTop: 20,
+                marginBottom: 10,
+            }}
+        >
+            <a href="/">
+                <img src={logoUrl} height={64} width={64} alt="logo" />
+            </a>
+        </div>
+    )
 }
