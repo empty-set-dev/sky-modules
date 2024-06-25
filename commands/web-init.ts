ffa73b334b681bd3dae1ba5ac0aee20e4d582c46
Анна Севрюкова <anyasky91@yandex.ru>
2024-06-25 12:19:58 +0300

    Fixes

---
+++ commands/web-init.ts
@@ -0,0 +1,46 @@
+#!/usr/bin/env -S npx tsx
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+import args from 'args'
+
+import __loadSkyConfig, { __getAppConfig } from './__loadSkyConfig'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
+args.parse(process.argv, {
+    name: 'sky web start',
+    mainColor: 'magenta',
+    subColor: 'grey',
+    mri: {},
+})
+
+export namespace web {
+    init()
+
+    export function init(): void {
+        const name = process.argv[4]
+
+        if (name == null || name === '') {
+            // eslint-disable-next-line no-console
+            console.error('missing app name')
+            // eslint-disable-next-line
+            return
+        }
+
+        const skyConfig = __loadSkyConfig()
+
+        if (!skyConfig) {
+            return
+        }
+
+        const skyAppConfig = __getAppConfig(name, skyConfig)
+
+        if (!skyAppConfig) {
+            return
+        }
+
+        fs.cpSync(path.resolve(__dirname, 'web-initial'), 'tests/web', { recursive: true })
+    }
+}
--- tests/web/pages/index/+Page.tsx
+++ tests/web/pages/index/+Page.tsx
@@ -1,18 +1,5 @@
-import { Counter } from './Counter'
-
 import { PageLayout } from '@/layouts/PageLayout'
 
 export function Page(): ReactNode {
-    return (
-        <PageLayout>
-            <h1>Welcome</h1>
-            This page is:
-            <ul>
-                <li>Rendered to HTML.</li>
-                <li>
-                    Interactive. <Counter />
-                </li>
-            </ul>
-        </PageLayout>
-    )
+    return <PageLayout>Hello, World!</PageLayout>
 }
