df6b477f9167963fe26e6bf186d059163b2a70fa
Анна Севрюкова <anyasky91@yandex.ru>
2024-06-25 14:44:37 +0300

    Fixes

--- commands/init-package.ts
+++ commands/init-package.ts
@@ -1,8 +1,12 @@
 #!/usr/bin/env -S npx tsx
 import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 import { b, e, purple } from './__coloredConsole'
 import __loadSkyConfig from './__loadSkyConfig'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 export namespace init {
     const nodeCommands = ['dev', 'build', 'start']
@@ -43,54 +47,68 @@ export namespace init {
         }
 
         packageJson.scripts = { ...(skyConfig.scripts as Record<string, string>) }
-        skyConfig.apps.forEach(app => {
-            if (app.target === 'node') {
-                nodeCommands.forEach(
-                    command =>
-                        (packageJson.scripts[
-                            `${app.name}:node:${command}`
-                        ] = `sky node ${command} ${app.name}`)
-                )
-            }
 
-            if (app.target === 'native' || app.target === 'universal' || app.target === 'desktop') {
-                tauriCommands.forEach(
-                    command =>
-                        (packageJson.scripts[
-                            `${app.name}:desktop:${command}`
-                        ] = `sky desktop ${command} ${app.name}`)
-                )
-            }
+        if (process.cwd() !== path.resolve(__dirname, '../')) {
+            skyConfig.apps.forEach(app => {
+                if (app.target === 'node') {
+                    nodeCommands.forEach(
+                        command =>
+                            (packageJson.scripts[
+                                `${app.name}:node:${command}`
+                            ] = `sky node ${command} ${app.name}`)
+                    )
+                }
 
-            if (app.target === 'native' || app.target === 'universal' || app.target === 'mobile') {
-                mobileCommands.forEach(
-                    command =>
-                        (packageJson.scripts[
-                            `${app.name}:${command.replaceAll(' ', ':')}`
-                        ] = `sky ${command} ${app.name}`)
-                )
-            }
+                if (
+                    app.target === 'native' ||
+                    app.target === 'universal' ||
+                    app.target === 'desktop'
+                ) {
+                    tauriCommands.forEach(
+                        command =>
+                            (packageJson.scripts[
+                                `${app.name}:desktop:${command}`
+                            ] = `sky desktop ${command} ${app.name}`)
+                    )
+                }
 
-            if (app.target === 'universal') {
-                webCommands.forEach(
-                    command =>
-                        (packageJson.scripts[
-                            `${app.name}:web:${command}`
-                        ] = `sky web ${command} ${app.name}`)
-                )
-            }
+                if (
+                    app.target === 'native' ||
+                    app.target === 'universal' ||
+                    app.target === 'mobile'
+                ) {
+                    mobileCommands.forEach(
+                        command =>
+                            (packageJson.scripts[
+                                `${app.name}:${command.replaceAll(' ', ':')}`
+                            ] = `sky ${command} ${app.name}`)
+                    )
+                }
 
-            if (app.target === 'web') {
-                webCommands.forEach(
-                    command =>
-                        (packageJson.scripts[
-                            `${app.name}:web:${command}`
-                        ] = `sky web ${command} ${app.name}`)
-                )
-            }
-        })
-        packageJson.scripts['format'] = 'sky format'
-        packageJson.scripts['test'] = 'sky test'
+                if (app.target === 'universal') {
+                    webCommands.forEach(
+                        command =>
+                            (packageJson.scripts[
+                                `${app.name}:web:${command}`
+                            ] = `sky web ${command} ${app.name}`)
+                    )
+                }
+
+                if (app.target === 'web') {
+                    webCommands.forEach(
+                        command =>
+                            (packageJson.scripts[
+                                `${app.name}:web:${command}`
+                            ] = `sky web ${command} ${app.name}`)
+                    )
+                }
+            })
+
+            packageJson.scripts['format'] = 'sky format'
+            packageJson.scripts['test'] = 'sky test'
+        } else {
+            delete packageJson.scripts
+        }
 
         process.stdout.write(`${b}${purple}Rewrite package.json${e}`)
         fs.writeFileSync('package.json', JSON.stringify(packageJson, null, '    '), 'utf-8')
