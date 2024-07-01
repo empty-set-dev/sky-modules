1305cb921d085da3c6718a1ac7aac5c94d7845f0
Анна Севрюкова <anyasky91@yandex.ru>
2024-07-01 07:31:59 +0300

    Fixes

---
+++ libs/three/-three.d.ts
@@ -0,0 +1,4 @@
+import Three from 'three'
+
+export = Three
+export as namespace Three
--- libs/three/global.ts
+++ libs/three/global.ts
@@ -1,11 +1,15 @@
 import globalify from 'sky/helpers/globalify'
-import { Object3D, Scene } from 'three/src/Three'
-import * as module from 'three/src/Three'
+import * as module from 'three'
+import { Scene, Object3D } from 'three'
 
 declare global {
     class InScene extends Effect {
         constructor(object: Object3D, scene: Scene, deps: EffectDeps)
     }
+
+    class Scene extends module.Scene {}
+    class Object3D extends module.Object3D {}
+    class PerspectiveCamera extends module.PerspectiveCamera {}
 }
 
 @effect
