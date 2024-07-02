0d13695f2678b6efdaa22e032888ae0e6dc421cb
Анна Севрюкова <anyasky91@yandex.ru>
2024-07-02 10:52:51 +0300

    Fixes

--- geometries/TiledPlaneGeometry.ts
+++ geometries/TiledPlaneGeometry.ts
@@ -17,11 +17,17 @@ export default class TiledPlaneGeometry extends PlaneGeometry {
         const uvAttribute = this.attributes.uv
 
         for (let i = 0; i < uvAttribute.count; i++) {
-            const u = uvAttribute.getX(i)
-            const v = uvAttribute.getY(i)
-
             const f = mapFactor
-            uvAttribute.setXY(i, (u + x) * f, (v + y) * f)
+
+            if (i === 0) {
+                uvAttribute.setXY(i, (x - width / 2) * f, (y + depth / 2) * f)
+            } else if (i === 1) {
+                uvAttribute.setXY(i, (x + width / 2) * f, (y + depth / 2) * f)
+            } else if (i === 2) {
+                uvAttribute.setXY(i, (x - width / 2) * f, (y - depth / 2) * f)
+            } else if (i === 3) {
+                uvAttribute.setXY(i, (x + width / 2) * f, (y - depth / 2) * f)
+            }
         }
     }
 }
