885b12261eadae3932a8b5f67b7e15eb153662f1
Анна Севрюкова <anyasky91@yandex.ru>
2024-07-05 11:02:51 +0300

    Rework

--- geometries/TiledBoxGeometry.ts
+++ geometries/TiledBoxGeometry.ts
@@ -16,48 +16,48 @@ export default class TiledBoxGeometry extends BoxGeometry {
         mapFactor ??= 1
         super(width, depth, height)
 
-        const uvAttribute = this.attributes.uv
+        const uvAttributes = this.attributes.uv
 
-        for (let i = 0; i < uvAttribute.count; i++) {
-            const u = uvAttribute.getX(i)
-            const v = uvAttribute.getY(i)
+        for (let i = 0; i < uvAttributes.count; i++) {
+            const u = uvAttributes.getX(i)
+            const v = uvAttributes.getY(i)
 
             const f = mapFactor
 
             if (i === 0) {
-                uvAttribute.setXY(i, (z + height / 2) * f, (v + y) * f)
+                uvAttributes.setXY(i, (z + height / 2) * f, (v + y) * f)
             } else if (i === 1) {
-                uvAttribute.setXY(i, (z - height / 2) * f, (v + y) * f)
+                uvAttributes.setXY(i, (z - height / 2) * f, (v + y) * f)
             } else if (i === 2) {
-                uvAttribute.setXY(i, (z + height / 2) * f, (v + y) * f)
+                uvAttributes.setXY(i, (z + height / 2) * f, (v + y) * f)
             } else if (i === 3) {
-                uvAttribute.setXY(i, (z - height / 2) * f, (v + y) * f)
+                uvAttributes.setXY(i, (z - height / 2) * f, (v + y) * f)
             } else if (i === 4) {
-                uvAttribute.setXY(i, (z + height / 2) * f, (v + y) * f)
+                uvAttributes.setXY(i, (z + height / 2) * f, (v + y) * f)
             } else if (i === 5) {
-                uvAttribute.setXY(i, (z - height / 2) * f, (v + y) * f)
+                uvAttributes.setXY(i, (z - height / 2) * f, (v + y) * f)
             } else if (i === 6) {
-                uvAttribute.setXY(i, (z + height / 2) * f, (v + y) * f)
+                uvAttributes.setXY(i, (z + height / 2) * f, (v + y) * f)
             } else if (i === 7) {
-                uvAttribute.setXY(i, (z - height / 2) * f, (v + y) * f)
+                uvAttributes.setXY(i, (z - height / 2) * f, (v + y) * f)
             } else if (i === 8) {
-                uvAttribute.setXY(i, (u + x) * f, (z - height / 2) * f)
+                uvAttributes.setXY(i, (u + x) * f, (z - height / 2) * f)
             } else if (i === 9) {
-                uvAttribute.setXY(i, (u + x) * f, (z - height / 2) * f)
+                uvAttributes.setXY(i, (u + x) * f, (z - height / 2) * f)
             } else if (i === 10) {
-                uvAttribute.setXY(i, (u + x) * f, (z + height / 2) * f)
+                uvAttributes.setXY(i, (u + x) * f, (z + height / 2) * f)
             } else if (i === 11) {
-                uvAttribute.setXY(i, (u + x) * f, (z + height / 2) * f)
+                uvAttributes.setXY(i, (u + x) * f, (z + height / 2) * f)
             } else if (i === 12) {
-                uvAttribute.setXY(i, (u + x) * f, (z - height / 2) * f)
+                uvAttributes.setXY(i, (u + x) * f, (z - height / 2) * f)
             } else if (i === 13) {
-                uvAttribute.setXY(i, (u + x) * f, (z - height / 2) * f)
+                uvAttributes.setXY(i, (u + x) * f, (z - height / 2) * f)
             } else if (i === 14) {
-                uvAttribute.setXY(i, (u + x) * f, (z + height / 2) * f)
+                uvAttributes.setXY(i, (u + x) * f, (z + height / 2) * f)
             } else if (i === 15) {
-                uvAttribute.setXY(i, (u + x) * f, (z + height / 2) * f)
+                uvAttributes.setXY(i, (u + x) * f, (z + height / 2) * f)
             } else {
-                uvAttribute.setXY(i, (u + x) * f, (v + y) * f)
+                uvAttributes.setXY(i, (u + x) * f, (v + y) * f)
             }
         }
     }
