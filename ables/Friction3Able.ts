53ff5e758e83267e70d8e38fac08f54fbfbcd2e0
Анна Севрюкова <anyasky91@yandex.ru>
2024-07-12 11:46:32 +0300

    Fixes

--- ables/Friction3Able.ts
+++ ables/Friction3Able.ts
@@ -3,6 +3,10 @@ import 'sky/features/ecs/global'
 export default class Friction3Able extends Component {
     friction: MetersPerSecond
 
+    /**
+     * @param {Entity} entity
+     * @param {MetersPerSecond} friction
+     */
     constructor(entity: Entity, friction: MetersPerSecond) {
         super(entity)
 
