532f5a40c18cbd3d1ba68e066b1f1a3f3a25dce6
Анна Севрюкова <anyasky91@yandex.ru>
2024-07-15 12:12:41 +0300

    Fixes

--- helpers/EventEmitter/tests/EventEmitter.test.ts
+++ helpers/EventEmitter/tests/EventEmitter.test.ts
@@ -1,8 +1,10 @@
 /* eslint-disable no-console */
 import 'helpers/EventEmitter/global'
 
-const eventEmitter = new EventEmitter()
-const dispose = eventEmitter.on('test', () => console.log('test event fired once'))
-eventEmitter.emit('test')
-dispose()
-eventEmitter.emit('test')
+test('EventEmitter', () => {
+    const eventEmitter = new EventEmitter()
+    const dispose = eventEmitter.on('test', () => console.log('test event fired once'))
+    eventEmitter.emit('test')
+    dispose()
+    eventEmitter.emit('test')
+})
