765947494d947ed4ba62f7536d76832a7ed6cc44
Анна Севрюкова <anyasky91@yandex.ru>
2024-07-22 13:04:13 +0300

    Fixes

--- commands/init-sky-config.ts
+++ commands/init-sky-config.ts
@@ -1,27 +1,20 @@
 #!/usr/bin/env -S npx tsx
 import fs from 'fs'
+import path from 'path'
 
 import { b, e, purple } from './__coloredConsole'
-import { SkyConfig } from './__loadSkyConfig'
+import __sdkPath from './__sdkPath'
 
 export namespace init {
     tsconfig()
 
     export function tsconfig(): void {
-        const skyConfigJson: SkyConfig = fs.existsSync('sky.config.json')
-            ? JSON.parse(fs.readFileSync('sky.config.json', 'utf-8'))
-            : {
-                  apps: {},
-                  modules: {},
-                  scripts: {},
-              }
+        if (fs.existsSync('sky.config.ts')) {
+            return
+        }
 
-        skyConfigJson.apps ??= []
-        skyConfigJson.modules ??= []
-        skyConfigJson.scripts ??= {}
-
-        process.stdout.write(`${b}${purple}Rewrite sky.config.json${e}`)
-        fs.writeFileSync('sky.config.json', JSON.stringify(skyConfigJson, null, '    '), 'utf-8')
+        process.stdout.write(`${b}${purple}Add sky.config.ts${e}`)
+        fs.copyFileSync(path.join(__sdkPath, 'commands/configs/sky.config.ts'), 'sky.config.ts')
         process.stdout.write(` 👌\n`)
     }
 }
