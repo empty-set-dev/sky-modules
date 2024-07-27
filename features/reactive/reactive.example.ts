9bd43d4c58dec0abe3caf3535ab2807d69e6badc
Анна Севрюкова <anyasky91@yandex.ru>
2024-07-27 13:06:12 +0300

    Fix

--- features/reactive/reactive.example.ts
+++ features/reactive/reactive.example.ts
@@ -1,21 +1,26 @@
-import '.'
+import './global'
 
 class Foo {
     @reactive
     x: number = reactive(
         () => {
             this.y
+            this.z
         },
-        () => this.y * this.y
+        () => this.y * this.z
     )
 
     @reactive
-    y: number = 0
+    y: number = 1
+
+    @reactive
+    z: number = 1
 }
 
 const foo = new Foo()
 
 foo.y = 10
+foo.z = 10
 
 // eslint-disable-next-line no-console
 console.log(foo.x)
