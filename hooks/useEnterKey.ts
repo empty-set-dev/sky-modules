dc95bf5b20be1a036c190e580429587753701dad
Анна Севрюкова <a.sevrukova@getic.com>
2024-09-14 15:03:33 +0300

    onEnterKey

---
+++ hooks/useEnterKey.ts
@@ -0,0 +1,19 @@
+import { DependencyList } from 'react'
+
+export default function useEnterKey(callback: () => void, deps: DependencyList): void {
+    useEffect(() => {
+        function handleKey(e: KeyboardEvent): void {
+            if (e.key === 'Enter') {
+                e.preventDefault()
+                callback()
+            }
+        }
+
+        window.addEventListener('keydown', handleKey)
+
+        return (): void => {
+            window.removeEventListener('keydown', handleKey)
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [deps])
+}
