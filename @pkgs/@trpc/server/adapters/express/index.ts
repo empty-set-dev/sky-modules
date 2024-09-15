3218dbd605024b7b1e5516dfd2ba0cf6c5432057
Анна Севрюкова <a.sevrukova@getic.com>
2024-09-15 20:21:07 +0300

    Fixes

--- @pkgs/@trpc/server/adapters/express/index.ts
+++ @pkgs/@trpc/server/adapters/express/index.ts
@@ -1,3 +1,1 @@
-import { CreateExpressContextOptions, createExpressMiddleware } from '@trpc/server/adapters/express'
-
-export { CreateExpressContextOptions, createExpressMiddleware }
+export * from '@trpc/server/adapters/express'
