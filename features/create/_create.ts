d98588b0a9251224734fdbadb4b0f7b703760731
Анна Севрюкова <a.sevrukova@getic.com>
2024-09-22 15:54:58 +0300

    Fixes

--- features/create/_create.ts
+++ features/create/_create.ts
@@ -1,12 +1,54 @@
 export {}
 
 declare global {
-    function create(): Promise<void>
+    class WithCreate {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        constructor(NewIsProhibited: NewIsProhibited)
+
+        set destroy(fn: () => Promise<void>)
+    }
+
+    function create<
+        Instance,
+        Args extends unknown[],
+        T extends Class<
+            Instance & {
+                create?(...args: Args): Promise<void>
+            } & WithCreate,
+            [NewIsProhibited]
+        >
+    >(class_: T, ...args: Args): Promise<InstanceType<T>>
+}
+
+abstract class NewIsProhibited {
+    private NewIsProhibitedId!: void
 }
 
 namespace lib {
-    export async function create<T extends Class>(): Promise<void> {
-        //
+    export class WithCreate {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        constructor(NewIsProhibited: NewIsProhibited) {
+            //
+        }
+
+        set destroy(fn: () => Promise<void>) {
+            this.destroy = fn
+        }
+    }
+
+    export async function create<
+        Instance,
+        Args extends unknown[],
+        T extends Class<
+            Instance & {
+                create?(...args: Args): Promise<void>
+            } & WithCreate,
+            [NewIsProhibited]
+        >
+    >(class_: T, ...args: Args): Promise<InstanceType<T>> {
+        const instance = new class_(null as never)
+        instance.create && (await instance.create(...args))
+        return instance as InstanceType<T>
     }
 }
 
