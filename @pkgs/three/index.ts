28dcd05afd54dadaef26ac41f128276cec3002fd
Анна Севрюкова <a.sevrukova@getic.com>
2024-09-26 10:43:40 +0300

    Fixes

--- @pkgs/three/index.ts
+++ @pkgs/three/index.ts
@@ -5,7 +5,6 @@ import * as Three from './_three.d'
 export * from './_three.d'
 export default Three
 
-@effect
 export class InScene extends Effect {
     constructor(object: Three.Object3D, scene: Three.Scene, deps: EffectDeps) {
         super(deps)
--- features/asyncConstructor/_asyncConstructor.ts
+++ features/asyncConstructor/_asyncConstructor.ts
@@ -2,12 +2,12 @@ import globalify from 'sky/helpers/globalify'
 
 declare global {
     //@ts-ignore
-    async function async<T>(fn: () => Promise<T>): T
+    async function asyncConstructor<T>(fn: () => Promise<T>): T
 }
 
 namespace lib {
     //@ts-ignore
-    export async function async<T>(fn: () => Promise<T>): T {
+    export async function asyncConstructor<T>(fn: () => Promise<T>): T {
         return await fn()
     }
 }
