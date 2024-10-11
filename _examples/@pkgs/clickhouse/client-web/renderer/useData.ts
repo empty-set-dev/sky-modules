b5ac6ad585f793716f89e6caa48930f6b034e3c5
Анна Севрюкова <a.sevrukova@getic.com>
2024-10-11 15:15:36 +0300

    Fixes

---
+++ _examples/@pkgs/clickhouse/client-web/renderer/useData.ts
@@ -0,0 +1,8 @@
+// https://vike.dev/useData
+import { usePageContext } from './usePageContext'
+
+/** https://vike.dev/useData */
+export function useData<Data>(): Data {
+    const { data } = usePageContext()
+    return data as Data
+}
