d292a05c402c6ae506c2545d85d69acc722848c1
Анна Севрюкова <a.sevrukova@getic.com>
2024-10-17 17:53:13 +0300

    Initials done

--- _examples/@platform/universal/App.tsx
+++ _examples/@platform/universal/App.tsx
@@ -1,18 +1,27 @@
 import { View, Text } from 'react-native'
 
-export default function App(): ReactNode {
-    return (
-        <View
-            style={{
-                width: '100%',
-                height: '100%',
-                justifyContent: 'center',
-                alignItems: 'center',
-            }}
-        >
-            <Text style={{ color: 'inherit' }}>
-                Universal React with <b>Vite</b>, <b>Tauri</b> and <b>Expo</b>
-            </Text>
-        </View>
-    )
+export default class App extends EffectsRoot {
+    static context = true
+
+    constructor() {
+        super()
+    }
+
+    @bind
+    UI(): ReactNode {
+        return (
+            <View
+                style={{
+                    width: '100%',
+                    height: '100%',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <Text style={{ color: 'inherit' }}>
+                    Universal React with <b>Vite</b>, <b>Tauri</b> and <b>Expo</b>
+                </Text>
+            </View>
+        )
+    }
 }
