481dd4cf4820313ec6c796f35ce7ff1cb4fc5c20
Анна Севрюкова <a.sevrukova@getic.com>
2024-10-24 01:48:55 +0300

    Fixes

--- @pkgs/three/index.ts
+++ @pkgs/three/index.ts
@@ -5,7 +5,7 @@ import * as Three from './_three.d'
 export * from './_three.d'
 export default Three
 
-export function inScene(object: Three.Object3D, scene: Three.Scene, deps: EffectDeps) {
+export function inScene(object: Three.Object3D, scene: Three.Scene, deps: EffectDeps): Effect {
     return new Effect(() => {
         scene.add(object)
 
