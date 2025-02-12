dbd091d4ccd0faae44ce80cc6c8c30ab46979ec0
Анна Севрюкова <a.sevrukova@getic.com>
2025-02-12 22:13:35 +0300

    Fixes

--- pkgs/three/index.ts
+++ pkgs/three/index.ts
@@ -1,8 +1,8 @@
 import 'sky/features/effect/global'
 
-import * as Three from './_three'
+import * as Three from './_three.d'
 
-export * from './_three'
+export * from './_three.d'
 export default Three
 
 export function inScene(scene: Three.Scene, object: Three.Object3D, deps: EffectDeps): Effect {
