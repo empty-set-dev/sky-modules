bc0a0a6dcd35aebcdf2c158e72e5509418954823
Ann Sky <68539094+ann-sky-91@users.noreply.github.com>
2025-02-16 15:21:25 +0300

    Ui

--- _examples/ui/index.ts
+++ _examples/ui/index.ts
@@ -1,3 +1,51 @@
 import '#/imports'
+import SkyPerspectiveCamera from 'sky/cameras/SkyPerspectiveCamera'
+import SkyRenderer from 'sky/renderers/SkyRenderer'
 
-class App extends EffectsRoot {}
+import styles from './index.module.scss'
+const cx = cn('[index]', styles)
+
+export class App extends EffectsRoot {
+    renderer: SkyRenderer
+    camera: SkyPerspectiveCamera
+    scene: Three.Scene
+
+    constructor() {
+        super()
+
+        const renderer = (this.renderer = new SkyRenderer(this, {
+            size: (): [number, number] => [window.innerWidth, window.innerHeight],
+        }))
+        renderer.setClearColor('#2b2b2b', 1.0)
+
+        const canvas = renderer.domElement
+        document.querySelector('#root')!.before(canvas)
+        cx`index-canvas` && canvas.classList.add(cx`index-canvas`)
+
+        const camera = (this.camera = new SkyPerspectiveCamera(this))
+
+        camera.position.z = 4
+
+        const scene = (this.scene = new Three.Scene())
+
+        const timer = new Timer()
+
+        new AnimationFrames(() => {
+            renderer.render(scene, camera)
+            const dt = timer.time()
+
+            this.emit('beforeUpdate', dt)
+            this.emit('update', dt)
+            this.emit('afterUpdate', dt)
+            this.emit('beforeAnimationFrame', dt)
+            this.emit('onAnimationFrame', dt)
+            this.emit('afterAnimationFrame', dt)
+        }, this)
+
+        //
+        const button = new UI.Button(this)
+        inScene(this.scene, button.view, [this, button])
+    }
+}
+
+new App()
