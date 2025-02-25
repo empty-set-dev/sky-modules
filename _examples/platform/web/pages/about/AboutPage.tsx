d22877c4ac75076b5d77cd26a37874ca69017844
Анна Севрюкова <a.sevrukova@getic.com>
2025-02-25 17:03:25 +0300

    Fixes

--- _examples/platform/web/pages/about/AboutPage.tsx
+++ _examples/platform/web/pages/about/AboutPage.tsx
@@ -14,8 +14,8 @@ export function AboutPage(): ReactNode {
     return (
         <PageLayout>
             <div className={cx`AboutPage`}>
-                <h1 className={cx`title`}>About</h1>
-                <p className={cx`text`}>Example of using Sky.</p>
+                <h1 className={cx`e:title`}>About</h1>
+                <p className={cx`e:text`}>Example of using Sky.</p>
             </div>
         </PageLayout>
     )
