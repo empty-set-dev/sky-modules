6ccb1770b2926604cb4c58dec5235f7939df00d6
Анна Севрюкова <a.sevrukova@getic.com>
2025-03-01 10:54:35 +0300

    Fixes

--- helpers/console/global.ts
+++ helpers/console/global.ts
@@ -8,6 +8,8 @@ declare global {
     function errorConsole(message?: unknown, ...optionalParams: unknown[]): void
     function infoConsole(message?: unknown, ...optionalParams: unknown[]): void
     function logConsole(message?: unknown, ...optionalParams: unknown[]): void
+    function timeConsole(label?: string): void
+    function timeEndConsole(label?: string): void
     function traceConsole(message?: unknown, ...optionalParams: unknown[]): void
     function debugConsole(message?: unknown, ...optionalParams: unknown[]): void
     function tableConsole(...data: unknown[]): void
