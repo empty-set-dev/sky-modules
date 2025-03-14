270aea4943cf294d6d220a3b4b964baa2cce086e
Анна Севрюкова <a.sevrukova@getic.com>
2025-03-14 11:50:28 +0300

    just try

---
+++ helpers/justTry/_justTry.ts
@@ -0,0 +1,8 @@
+export default async function justTry<T>(fn: () => T): Promise<undefined | T> {
+    try {
+        return await fn()
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (err) {
+        //
+    }
+}
---
+++ helpers/justTry/index.ts
@@ -0,0 +1,2 @@
+export * from './_justTry'
+export { default } from './_justTry'
