7d123b1a50891ab0ae07d36bc4b433b608a64513
Анна Севрюкова <a.sevrukova@getic.com>
2025-03-14 12:56:01 +0300

    Fixes

--- helpers/justTry/global.ts
+++ helpers/justTry/global.ts
@@ -1,7 +1,9 @@
-import globalify from '../globalify'
+import globalify from 'sky/helpers/globalify'
+
+import * as pkg from '.'
+
+globalify({ pkg })
 
 declare global {
     function justTry<T>(fn: () => T): Promise<undefined | T>
 }
-
-globalify({ justTry })
