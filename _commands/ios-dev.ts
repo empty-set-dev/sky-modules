153bd4b65bc8f5100bef0f3eb2458cc114c95fd3
Анна Севрюкова <a.sevrukova@getic.com>
2025-04-12 13:15:17 +0300

    Fixes

---
+++ _commands/ios-dev.ts
@@ -0,0 +1,36 @@
+#!/usr/bin/env -S pnpm exec tsx
+import path from 'path'
+
+import { errorConsole } from '../utilities/console'
+
+import __getAppEntry from './__getAppEntry'
+import __loadSkyConfig, { __getAppConfig } from './__loadSkyConfig'
+import __run from './__run'
+import __sdkPath from './__sdkPath'
+
+devIos()
+
+async function devIos(): Promise<void> {
+    const name = process.argv[4]
+
+    if (name == null || name === '') {
+        errorConsole('missing app name')
+        return
+    }
+
+    const skyConfig = await __loadSkyConfig()
+
+    if (!skyConfig) {
+        return
+    }
+
+    const skyAppConfig = __getAppConfig(name, skyConfig)
+
+    if (!skyAppConfig) {
+        return
+    }
+
+    __run(path.resolve(__sdkPath, 'node_modules/.bin/expo'), {
+        cwd: path.resolve(skyAppConfig.path),
+    })
+}
---
+++ _commands/ios.ts
@@ -0,0 +1,30 @@
+#!/usr/bin/env -S pnpm exec tsx
+/* eslint-disable @typescript-eslint/no-empty-function */
+import args from 'args'
+
+import { errorConsole } from '../utilities/console'
+
+import __import from './__import'
+
+function initArgs(): void {
+    args.command('dev', 'Dev', () => {})
+    args.command('build', 'Build', () => {})
+    args.command('start', 'Start', () => {})
+
+    args.parse(process.argv, {
+        name: 'sky ios',
+        mainColor: 'magenta',
+        subColor: 'grey',
+        mri: {},
+    })
+}
+
+const command = process.argv[3]
+if (!command) {
+    initArgs()
+    args.showHelp()
+} else if (!__import(`./ios-${command}.ts`)) {
+    initArgs()
+    errorConsole(`ios: command "${command}" not found`)
+    args.showHelp()
+}
