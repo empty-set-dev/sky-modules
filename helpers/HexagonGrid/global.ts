5502cfc7712bdb332153b0177018e29a05e756fb
Анна Севрюкова <a.sevrukova@getic.com>
2025-05-01 18:42:23 +0300

    Update

--- helpers/HexagonGrid/global.ts
+++ helpers/HexagonGrid/global.ts
@@ -6,6 +6,8 @@ globalify({ Hexagon: pkg })
 
 declare global {
     namespace Hexagon {
+        type Hexagon = pkg.Hexagon
+        const Hexagon: typeof pkg.Hexagon
         class Grid extends pkg.Grid {}
         interface CircleParameters extends pkg.CircleParameters {}
         class Circle extends pkg.Circle {}
