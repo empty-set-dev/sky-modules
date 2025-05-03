c40eca0bb4580b624be8d91371724414253cd7d3
Анна Севрюкова <a.sevrukova@getic.com>
2025-05-03 13:29:14 +0300

    Update

---
+++ _examples/features/ecs/imports.ts
@@ -0,0 +1,10 @@
+import 'sky/platform/node/global'
+import 'sky/math/global'
+import 'sky/utilities/global'
+import 'sky/helpers/global'
+
+import 'sky/features/effect/global'
+import 'sky/features/ecs/global'
+
+import 'sky/ecs-components/global'
+import 'sky/ecs-systems/global'
