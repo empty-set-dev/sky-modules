ba23e7598f7c35ee2655ff69cc6da34bce873890
Анна Севрюкова <a.sevrukova@getic.com>
2025-05-03 13:23:41 +0300

    Fixes

--- platform/web/renderer/PageProviders.tsx
+++ platform/web/renderer/PageProviders.tsx
@@ -1,3 +1,4 @@
+// @ts-expect-error
 import { HydrationBoundary, QueryClient, QueryClientProvider } from 'pkgs/@tanstack/react-query'
 import { PropsWithChildren, ReactNode, useEffect } from 'react'
 import { PageContextProvider } from 'sky/platform/web/contexts/PageContext'
