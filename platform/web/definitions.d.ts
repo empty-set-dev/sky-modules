f3989055fd419e8d0ca7859f141d98abed6e1b41
Anya Sky <anyasky91@yandex.ru>
2025-05-11 14:44:10 +0300

    Update

--- platform/web/definitions.d.ts
+++ platform/web/definitions.d.ts
@@ -1,3 +1,33 @@
+declare module '*.module.css' {
+    const classNames: Record<string, string>
+    export default classNames
+}
+
+declare module '*.css' {
+    const classNames: Record<string, string>
+    export default classNames
+}
+
+declare module '*.module.scss' {
+    const classNames: Record<string, string>
+    export default classNames
+}
+
+declare module '*.scss' {
+    const classNames: Record<string, string>
+    export default classNames
+}
+
+declare module '*.module.sass' {
+    const classNames: Record<string, string>
+    export default classNames
+}
+
+declare module '*.sass' {
+    const classNames: Record<string, string>
+    export default classNames
+}
+
 declare module '*.svg' {
     const content: string
     export default content
