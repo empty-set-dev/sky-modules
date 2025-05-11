c5af383c1a38982382eba5cd95244bae6afb7053
Анна Севрюкова <a.sevrukova@getic.com>
2025-05-11 13:12:54 +0300

    Update

---
+++ components/Page.tsx
@@ -0,0 +1,15 @@
+import classNames from 'pkgs/classnames'
+import { HTMLProps } from 'react'
+
+import Tag from './Tag'
+
+export interface PageProps<T> extends HTMLProps<T>, PropsWithChildren {
+    tag?: string
+}
+export default function Page<T>(props: PageProps<T>): ReactNode {
+    let { tag, className, ...tagProps } = props
+    tag ??= 'div'
+    className = classNames('Page', className)
+
+    return <Tag tag={tag} className={className} {...tagProps} />
+}
