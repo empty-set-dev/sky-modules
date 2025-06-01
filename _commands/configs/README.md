98822eecca16b2d4243de014452cd68e19f040db
Анна Севрюкова <a.sevrukova@getic.com>
2025-06-01 10:00:46 +0300

    Update

---
+++ _commands/configs/README.md
@@ -0,0 +1,12 @@
+# Sky App
+## Develop
+In projects folder
+```sh
+git clone https://github.com/empty-set-games/sky-modules
+```
+
+In this folder
+```sh
+pnpm i tsx
+npx tsx install
+```
--- _commands/init-packages.ts
+++ _commands/init-packages.ts
@@ -56,6 +56,10 @@ async function initPackages(): Promise<void> {
         fs.mkdirSync('.dev')
     }
 
+    if (!fs.existsSync('README.md')) {
+        fs.copyFileSync(path.join(__sdkPath, '_commands/configs/README.md'), 'README.md')
+    }
+
     fs.copyFileSync(
         path.join(__sdkPath, '_commands/configs/.dev/package.json'),
         '.dev/package.json'
