91b095a65dcb46fbe751117fd9814a729117e92e
Анна Севрюкова <a.sevrukova@getic.com>
2025-06-16 11:39:01 +0300

    update

--- _commands/desktop.ts
+++ _commands/desktop.ts
@@ -1,10 +1,19 @@
 #!/usr/bin/env -S pnpm exec tsx
 /* eslint-disable @typescript-eslint/no-empty-function */
 import args from 'args'
-
 import { errorConsole } from 'sky/utilities/console'
 
 import __import from './__import'
+
+const command = process.argv[3]
+if (!command) {
+    initArgs()
+    args.showHelp()
+} else if (!__import(`./desktop-${command}.ts`)) {
+    initArgs()
+    errorConsole(`desktop: command "${command}" not found`)
+    args.showHelp()
+}
 
 function initArgs(): void {
     args.command('init', 'Init', () => {})
@@ -17,14 +26,4 @@ function initArgs(): void {
         subColor: 'grey',
         mri: {},
     })
-}
-
-const command = process.argv[3]
-if (!command) {
-    initArgs()
-    args.showHelp()
-} else if (!__import(`./desktop-${command}.ts`)) {
-    initArgs()
-    errorConsole(`desktop: command "${command}" not found`)
-    args.showHelp()
 }
