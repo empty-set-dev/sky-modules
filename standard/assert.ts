289fec9e8dd9eacdcd5d970ae6b838a22fcc6f71
Анна Севрюкова <a.sevrukova@getic.com>
2025-06-23 15:22:47 +0300

    Update

--- standard/assert.ts
+++ standard/assert.ts
@@ -1,16 +1,20 @@
 import globalify from 'sky/utilities/globalify'
 
 declare global {
-    const assertion_failed: Error
+    class AssertionError extends lib.AssertionError {}
     function assert(expression: boolean): void
 }
 
 namespace lib {
-    export const assertion_failed = Error('assertion failed')
+    export class AssertionError extends Error {
+        constructor() {
+            super('assertion failed')
+        }
+    }
 
     export function assert(expression: boolean): void {
         if (!expression) {
-            throw assertion_failed
+            throw new AssertionError()
         }
     }
 }
