3d2c3f0c281ce4f264e27995ec17302fe7716da8
Анна Севрюкова <a.sevrukova@getic.com>
2025-06-24 13:28:00 +0300

    Update

--- components/UI/Modal.tsx
+++ components/UI/Modal.tsx
@@ -7,6 +7,8 @@ styles
 export interface ModalProps extends PropsWithChildren {
     className?: string
     effect?: Effect
+    closeOnClickOutside: boolean
+    close?: () => void
 }
 export default function Modal(props: ModalProps): ReactNode {
     const modalRoot = document.getElementById('modal-root')!
@@ -18,7 +20,19 @@ export default function Modal(props: ModalProps): ReactNode {
     }
 
     return createPortal(
-        <div className={`ModalWrapper`} {...captureProps}>
+        <div
+            className={`ModalWrapper`}
+            {...captureProps}
+            onClick={() => {
+                if (props.closeOnClickOutside) {
+                    if (isNull(props.close)) {
+                        throw new NullError()
+                    }
+
+                    props.close()
+                }
+            }}
+        >
             <div className={`Modal ${props.className}`}>{props.children}</div>
         </div>,
         modalRoot
