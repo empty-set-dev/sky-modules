ad2551b2b2b6b434c972b900da18cd71214d8a25
Анна Севрюкова <a.sevrukova@getic.com>
2025-08-15 20:12:47 +0300

    Update

--- utilities/EventEmitter/_EventEmitter-extend.ts
+++ utilities/EventEmitter/_EventEmitter-extend.ts
@@ -1,6 +1,6 @@
 import EventEmitter from './_EventEmitter'
 
-export default function extend<F>(fn: F): F & EventEmitter {
+EventEmitter.extend = function extend<F>(fn: F): F & EventEmitter {
     const prototype = Object.create(Object.getPrototypeOf(fn))
     Object.assign(prototype, {
         on: EventEmitter.prototype.on,
--- utilities/EventEmitter/_EventEmitter.ts
+++ utilities/EventEmitter/_EventEmitter.ts
@@ -1,21 +1,29 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import emit from './_EventEmitter+emit'
-import on from './_EventEmitter+on'
-import extend from './_EventEmitter-extend'
-
 export default class EventEmitter {
+    static super(): void {
+        //
+    }
     static extend: <F>(fn: F) => F & EventEmitter
 
-    on(ev: Object.Index, callback: (...args: unknown[]) => void): () => void {
+    on(ev: Object.Index, callback: (...args: unknown[]) => void): this {
+        ev
+        callback
+        return null!
+    }
+    off(ev: Object.Index, callback: (...args: unknown[]) => void): this {
+        ev
+        callback
         return null!
     }
     emit(ev: Object.Index, ...args: unknown[]): this {
+        ev
+        args
         return null!
     }
 
     private __events: Record<Object.Index, ((...args: unknown[]) => void)[]> = {}
 }
 
-EventEmitter.extend = extend
-EventEmitter.prototype.on = on
-EventEmitter.prototype.emit = emit
+import './_EventEmitter-extend'
+import './_EventEmitter+emit'
+import './_EventEmitter+off'
+import './_EventEmitter+on'
