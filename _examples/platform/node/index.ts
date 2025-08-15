25f72af7e22366f177c307c51a54f2efdcc9a68b
Анна Севрюкова <a.sevrukova@getic.com>
2025-08-15 20:25:45 +0300

    Update

--- _examples/platform/node/index.ts
+++ _examples/platform/node/index.ts
@@ -4,16 +4,47 @@ import 'sky/utilities/global'
 import 'sky/helpers/global'
 import 'sky/features/effect/global'
 
-class Test {
-    static context = true
+// class Test {
+//     static context = true
 
+//     readonly effect: Effect
+
+//     constructor(deps: EffectDeps) {
+//         this.effect = new Effect(deps, this)
+//     }
+// }
+
+const root = new EffectsRoot()
+// const test = new Test(root)
+// test
+
+interface Foo extends EventEmitter, Visibility {}
+@mixin(EventEmitter)
+@mixin(Visibility)
+class Foo {
     readonly effect: Effect
-
     constructor(deps: EffectDeps) {
+        EventEmitter.super(this)
+        Visibility.super(this)
+
         this.effect = new Effect(deps, this)
     }
 }
 
-const root = new EffectsRoot()
-const test = new Test(root)
-test
+const foo = new Foo(root)
+new Effect(() => {
+    function test(): void {
+        Console.log('test')
+    }
+
+    foo.on('test', test)
+    return (): void => {
+        foo.off('test', test)
+    }
+}, foo.effect)
+
+foo.emit('test')
+foo.effect.destroy()
+foo.emit('test')
+
+Console.log(foo.visible)
