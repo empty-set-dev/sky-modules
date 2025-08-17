b3585468a7b25c3c57e13b748c9aa3283374ee2f
Анна Севрюкова <a.sevrukova@getic.com>
2025-08-17 17:01:00 +0300

    Update

--- _examples/platform/node/index.ts
+++ _examples/platform/node/index.ts
@@ -4,60 +4,68 @@ import 'sky/utilities/global'
 import 'sky/helpers/global'
 import 'sky/features/effect/global'
 
-class Test {
-    @hook
-    draw(): void {
-        //
-    }
-}
-class Test2 extends Test {
-    @hook
-    draw(): void {
-        super.draw()
-    }
+const timer = new Timer()
+
+let start = Date.now()
+while (true) {
+    await timer.wait(Time(0.5))
+    Console.log(Date.now() - start)
 }
 
 // class Test {
-//     static context = true
+//     @hook
+//     draw(): void {
+//         //
+//     }
+// }
+// class Test2 extends Test {
+//     @hook
+//     draw(): void {
+//         super.draw()
+//     }
+// }
 
+// // class Test {
+// //     static context = true
+
+// //     readonly effect: Effect
+
+// //     constructor(deps: EffectDeps) {
+// //         this.effect = new Effect(deps, this)
+// //     }
+// // }
+
+// const root = new EffectsRoot()
+// // const test = new Test(root)
+// // test
+
+// interface Foo extends EventEmitter, Visibility {}
+// @mixin(EventEmitter)
+// @mixin(Visibility)
+// class Foo {
 //     readonly effect: Effect
-
 //     constructor(deps: EffectDeps) {
+//         EventEmitter.super(this)
+//         Visibility.super(this)
+
 //         this.effect = new Effect(deps, this)
 //     }
 // }
 
-const root = new EffectsRoot()
-// const test = new Test(root)
-// test
+// const foo = new Foo(root)
+// new Effect(() => {
+//     function test(): void {
+//         Console.log('test')
+//     }
 
-interface Foo extends EventEmitter, Visibility {}
-@mixin(EventEmitter)
-@mixin(Visibility)
-class Foo {
-    readonly effect: Effect
-    constructor(deps: EffectDeps) {
-        EventEmitter.super(this)
-        Visibility.super(this)
+//     foo.on('test', test)
+//     return (): void => {
+//         foo.off('test', test)
+//     }
+// }, foo.effect)
 
-        this.effect = new Effect(deps, this)
-    }
-}
+// foo.emit('test')
+// foo.effect.destroy()
+// foo.emit('test')
 
-const foo = new Foo(root)
-new Effect(() => {
-    function test(): void {
-        Console.log('test')
-    }
-
-    foo.on('test', test)
-    return (): void => {
-        foo.off('test', test)
-    }
-}, foo.effect)
-
-foo.emit('test')
-foo.effect.destroy()
-foo.emit('test')
-
-Console.log(foo.visible, root)
+// Console.log(foo.visible, root)
--- _examples/platform/universal/index.tsx
+++ _examples/platform/universal/index.tsx
@@ -9,3 +9,11 @@ import './index.scss'
 const app = new App()
 
 createRoot(document.getElementById('root')!).render(app.render())
+
+const timer = new Timer()
+
+let start = Date.now()
+while (true) {
+    await timer.wait(Time(0.5))
+    Console.log(Date.now() - start)
+}
