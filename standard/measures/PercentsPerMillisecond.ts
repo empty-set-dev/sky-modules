be450efa59051dd49b4e8c254eed00e0777e0fab
Анна Севрюкова <a.sevrukova@getic.com>
2025-08-23 14:59:44 +0300

    Update

--- standard/measures/PercentsPerMillisecond.ts
+++ standard/measures/PercentsPerMillisecond.ts
@@ -1,3 +1,16 @@
 import './_defineMeasures'
 
-defineMeasures('PercentsPerMillisecond', [])
+declare global {
+    interface Number {
+        get asPercentsPerMillisecond(): number
+    }
+    interface PercentsPerMillisecond extends Number, PercentsPerMillisecondID {
+        get inPercentsPerMillisecond(): number
+    }
+}
+
+class PercentsPerMillisecondID {
+    private PercentsPerMillisecondID = true
+}
+
+defineMeasures('PercentsPerMillisecond', [['PercentsPerMillisecond', 1]])
