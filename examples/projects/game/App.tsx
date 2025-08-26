b89d81d4d836c48e82ac822d934bcba41256edf0
Анна Севрюкова <a.sevrukova@getic.com>
2025-08-27 00:42:13 +0300

    Update

--- examples/projects/game/App.tsx
+++ examples/projects/game/App.tsx
@@ -1,23 +1,7 @@
 import '#/imports'
 
-import { Window } from '@tauri-apps/api/window'
 import { createRoot } from 'react-dom/client'
 import { Text, View } from 'react-native'
-
-const appWindow = new Window('theUniqueLabel')
-
-appWindow.once('tauri://created', function () {
-    // window successfully created
-})
-appWindow.once('tauri://error', function (e) {
-    // an error happened creating the window
-})
-
-// emit an event to the backend
-await appWindow.emit('some-event', 'data')
-// listen to an event from the backend
-const unlisten = await appWindow.listen('event-name', e => {})
-unlisten()
 
 @singleton
 @define('sky.examples.projects.game.App')
