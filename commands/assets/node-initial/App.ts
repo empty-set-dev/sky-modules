5fffefaccebd5039648e24e32031089a6b7f7fe4
Анна Севрюкова <a.sevrukova@getic.com>
2025-08-30 22:59:19 +0300

    Update

--- commands/assets/node-initial/App.ts
+++ commands/assets/node-initial/App.ts
@@ -3,5 +3,7 @@ import './imports'
 @define('${APP_ID}.App')
 @singleton
 export default class App {
-    //
+    async start(): Promise<void> {
+        Console.log('Hello, world!')
+    }
 }
--- examples/platform/node-project/app/App.ts
+++ examples/platform/node-project/app/App.ts
@@ -3,5 +3,7 @@ import './imports'
 @define('app.App')
 @singleton
 export default class App {
-    //
+    async create(): Promise<void> {
+        Console.log('Hello, world!')
+    }
 }
