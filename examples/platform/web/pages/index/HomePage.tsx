6924344dc446eb27d7ae52fdf25f0433f58b252b
Анна Севрюкова <a.sevrukova@getic.com>
2025-08-30 17:06:16 +0300

    Update

---
+++ examples/platform/web/pages/index/HomePage.tsx
@@ -0,0 +1,28 @@
+import '#/imports'
+import { observer } from 'mobx-react-lite'
+import { useTranslation } from 'react-i18next'
+import useData from 'sky/platform/web/renderer/useData'
+
+import PageLayout from '#/layouts/PageLayout'
+
+import HomePageData from './+data'
+import Counter from './Counter'
+import { onTest } from './HomePage.telefunc'
+
+export default observer(function HomePage(): ReactNode {
+    const { t } = useTranslation()
+
+    useData(HomePageData)
+
+    useEffect(() => {
+        async(onTest, 42)
+    }, [])
+
+    return (
+        <PageLayout>
+            {t`title`}
+            <br />
+            <Counter />
+        </PageLayout>
+    )
+})
