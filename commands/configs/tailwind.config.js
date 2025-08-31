0350592af1fe9873170a3ac71840435167a84516
Анна Севрюкова <a.sevrukova@getic.com>
2025-08-31 09:55:37 +0300

    Tailwind

--- commands/configs/tailwind.config.js
+++ commands/configs/tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
     theme: {
         screens: {
             sm: '576px',
-            md: '868px',
+            md: '768px',
             lg: '992px',
             xl: '1200px',
             xxl: '1400px',
