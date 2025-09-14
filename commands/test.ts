fced551ecd1fd97ecd4d85c78cbcf31103d035cd
Анна Севрюкова <a.sevrukova@getic.com>
2025-09-14 18:49:17 +0300

    Update

--- commands/test.ts
+++ commands/test.ts
@@ -3,13 +3,11 @@ import { ArgumentsCamelCase } from 'yargs'
 import runShell from './lib/run'
 import skyPath from './lib/skyPath'
 
-export default function test(argv: ArgumentsCamelCase<{ folder: string }>): void {
+export default async function test(argv: ArgumentsCamelCase<{ folder: string }>): Promise<void> {
     const folder = argv.folder ?? '.'
 
     try {
-        runShell(
-            `pnpm exec ${skyPath}/node_modules/.bin/jest --config ${skyPath}/commands/configs/jest.config.js ${folder}`
-        )
+        await runShell(`pnpm jest --config ${skyPath}/commands/configs/jest.config.js ${folder}`)
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (error: unknown) {
         //
