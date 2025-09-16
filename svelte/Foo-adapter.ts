8b85272601456a3d3ab51751098cabc0ebcd54d0
Анна Севрюкова <a.sevrukova@getic.com>
2025-09-16 18:09:08 +0300

    Update

--- svelte/Foo-adapter.ts
+++
@@ -1,9 +0,0 @@
-export { default } from './Foo'
-
-// Export JSX renderers
-export { default as MinimalJSX } from './minimal-jsx.svelte'
-export { default as SafeJSX } from './safe-jsx.svelte'
-
-// Export pre-compiled clean Svelte components (NO runtime overhead!)
-export { default as FooClean } from './FooCompiledClean.svelte'
-export { default as FooAuto } from './FooAutoCompiled.svelte'
