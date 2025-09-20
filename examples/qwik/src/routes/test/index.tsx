1625ef5f7ebb26b4f976f741a351028b87fd9ce2
Анна Севрюкова <a.sevrukova@getic.com>
2025-09-20 11:54:37 +0300

    Update

--- examples/qwik/src/routes/test/index.tsx
+++ examples/qwik/src/routes/test/index.tsx
@@ -2,9 +2,9 @@ import { $, component$, JSXOutput } from "@builder.io/qwik"
 import Box from "sky/qwik/Box"
 
 export default component$(() => {
-    return <Box as={Foo} some="123">test</Box>
+    return <Box as={Foo} sx="bg-blue-500 text-white p-4 rounded-lg" some="123">test</Box>
 })
 
 const Foo = $(function Foo(props: {some: string}): JSXOutput {
-    return <div>{props.some}</div>
+    return <div {...props}>{props.some}</div>
 })
