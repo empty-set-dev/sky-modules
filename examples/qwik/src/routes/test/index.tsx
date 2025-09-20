c5476a1043594b5fa4764e5ce76c3f888df484b3
Анна Севрюкова <a.sevrukova@getic.com>
2025-09-20 14:27:39 +0300

    Add CSS modules support and Tailwind plugin configuration

--- examples/qwik/src/routes/test/index.tsx
+++ examples/qwik/src/routes/test/index.tsx
@@ -1,7 +1,8 @@
 import { component$ } from "@builder.io/qwik"
 import Box from "sky/qwik/Box"
+import styles from './index.module.css'
 
 export default component$(() => {
-    return <Box as="section" sx="bg-blue-500 text-white p-4 rounded-lg">test</Box>
+    return <Box as="section" sx="bg-blue-500 text-white p-4 rounded-lg" class={styles.foo}>test</Box>
 })
 
