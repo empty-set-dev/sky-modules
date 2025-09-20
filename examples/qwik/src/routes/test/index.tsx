267b5af3505ee66b8062b44102233b7f0cab0359
Анна Севрюкова <a.sevrukova@getic.com>
2025-09-20 13:42:10 +0300

    Refactor SCSS absolute-center to use CSS placeholder and update stylelint config

--- examples/qwik/src/routes/test/index.tsx
+++ examples/qwik/src/routes/test/index.tsx
@@ -1,10 +1,7 @@
-import { $, component$, JSXOutput } from "@builder.io/qwik"
+import { component$ } from "@builder.io/qwik"
 import Box from "sky/qwik/Box"
 
 export default component$(() => {
-    return <Box as={Foo} sx="bg-blue-500 text-white p-4 rounded-lg" some="123">test</Box>
+    return <Box as="section" sx="bg-blue-500 text-white p-4 rounded-lg">test</Box>
 })
 
-const Foo = $(function Foo(props: {some: string}): JSXOutput {
-    return <div {...props}>{props.some}</div>
-})
