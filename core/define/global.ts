c4543b252301e568f4a9eb884cde0e1d6c9260a6
Анна Севрюкова <a.sevrukova@getic.com>
2025-09-26 09:14:06 +0300

    Apply code formatting and cleanup

--- core/define/global.ts
+++ core/define/global.ts
@@ -1,5 +1,4 @@
 import 'sky/core/runtime'
-import 'sky/core/modules.global'
 import 'sky/core/as'
 
 import './_define'
@@ -13,13 +12,8 @@ import './_types'
 import 'sky/core/task'
 
 import local from './__local'
-iAm('sky.standard.define', import('./global'))
 
 declare global {
-    interface Modules {
-        'sky.standard.define': typeof import('./global')
-    }
-
     interface Object {
         schema: Record<PropertyKey, unknown>
     }
@@ -34,7 +28,8 @@ task(async () => {
     await runtime
 
     const errors: string[] = []
-    Object.keys(local.loadedDefines).forEach(k => {
+
+    for (const k of Object.keys(local.loadedDefines)) {
         const define = local.defines[k]
 
         if (define == null) {
@@ -44,8 +39,9 @@ task(async () => {
 
         const id = local.loadedDefines[k]
         define.value[local.idSymbol] = id
-    })
-    Object.keys(local.defines).forEach(k => {
+    }
+
+    for (const k of Object.keys(local.defines)) {
         const define = local.loadedDefines[k]
 
         if (define == null) {
@@ -58,7 +54,7 @@ task(async () => {
         if (typeof value === 'object' || typeof value === 'function') {
             Object.freezeDeep(value)
         }
-    })
+    }
 
     if (errors.length > 0) {
         throw new Error(`\n    > ${errors.join('\n    > ')}`)
