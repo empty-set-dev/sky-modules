e131b75fedcb63fc221376de96bb76cbe11bfa09
Анна Севрюкова <a.sevrukova@getic.com>
2025-09-27 22:34:44 +0300

    Очистить boilerplates от старых глобальных деклараций
    - Убрать iAm() вызовы из всех шаблонов
    - Удалить global interface Modules декларации
    - Упростить boilerplates до базового экспорта
    - Заменить дефолтные значения на Hello world

--- boilerplates/global-single-module/{{GLOBAL_SINGLE_MODULE}}.ts
+++ boilerplates/global-single-module/{{GLOBAL_SINGLE_MODULE}}.ts
@@ -1,9 +1,1 @@
-iAm('{{GLOBAL_SINGLE_MODULE}}', import('./{{GLOBAL_SINGLE_MODULE}}'))
-
-declare global {
-    interface Modules {
-        '{{GLOBAL_SINGLE_MODULE}}': typeof import('./{{GLOBAL_SINGLE_MODULE}}')
-    }
-}
-
-export default 42
+export default 'Hello, world!'
--- boilerplates/module/index.ts
+++ boilerplates/module/index.ts
@@ -1,10 +1,2 @@
-iAm('{{MODULE}}', import('.'))
-
-declare global {
-    interface Modules {
-        '{{MODULE}}': typeof import('.')
-    }
-}
-
 export { default } from './_{{MODULE}}'
 export * from './_{{MODULE}}'
