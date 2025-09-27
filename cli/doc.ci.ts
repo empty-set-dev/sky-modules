2d31d7837dbb53c66cd62fb2714e66f2790d63c1
Анна Севрюкова <a.sevrukova@getic.com>
2025-09-27 12:56:41 +0300

    Update workflow to use pnpm sky doc command
    🔧 Simplified CI documentation generation command

--- cli/doc.ci.ts
+++
@@ -1,9 +0,0 @@
-import { generateDocsFromMarkdown, generateReadme } from './lib/index.ts'
-
-export default async function doc(): Promise<void> {
-    // Generate VitePress documentation
-    await generateDocsFromMarkdown()
-
-    // Generate unified README files
-    await generateReadme()
-}
