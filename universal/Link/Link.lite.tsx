eb456e8d1e9e297438679f61883646069c811db4
Anya Sky <anyasky91@yandex.ru>
2025-10-01 23:26:34 +0300

    Fix TypeScript errors and improve Box component type system
    - Update BoxProps type system to handle function components first
    - Add BoxAs type alias for better type inference
    - Fix SlotProps to use Exclude for undefined parameters
    - Update Link component to use new BoxAs constraint
    - Improve Box global types with proper conditional types
    - Fix import inconsistencies across Link components
    - Update tailwind-variants usage to slots pattern

--- universal/Link/Link.lite.tsx
+++ universal/Link/Link.lite.tsx
@@ -1,21 +1,21 @@
 import '@sky-modules/react/Box.global'
 import '@sky-modules/design/DesignSystem'
+import '@sky-modules/jsx/global'
 
 import { useRef, useStore } from '@builder.io/mitosis'
 import clsx from 'clsx'
 
 import getBoxProps from './Link.props.lite'
-import { LinkSx } from './Link.style.lite'
+import { link } from './Link.style.lite'
 
-export default function Link<T extends TagName = 'div'>(
-    props: DesignSystem.SlotProps<T, typeof LinkSx>
+export default function Link<T extends BoxAs = 'div'>(
+    props: DesignSystem.SlotProps<T, typeof LinkStyle>
 ): Mitosis.Node {
     const { sx, boxProps } = useStore({
-        boxProps: getBoxProps(props),
-        sx: LinkSx(props),
+        boxProps: getBoxProps<T>(props),
+        sx: LinkStyle(props),
     })
-    const inputRef = useRef<HTMLElement>(null)
-
+    const inputRef = useRef(null)
     return (
         <Box ref={inputRef} {...boxProps} as={(boxProps.as ?? 'a') as T} sx={clsx(sx, props.sx)}>
             {props.children}
