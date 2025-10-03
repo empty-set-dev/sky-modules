b9a895d095ebe28df77a804a092352c656f2416a
Анна Севрюкова <a.sevrukova@getic.com>
2025-10-03 09:00:56 +0300

    Fix Panda CSS integration with Vite
    - Update Panda config path in web.ts PostCSS plugin
    - Simplify panda.config.ts for minimal setup
    - Add Panda CSS styling to App.tsx with Box components
    - Enable watch mode and fix readonly property issues

--- examples/universal/App.tsx
+++ examples/universal/App.tsx
@@ -1,6 +1,9 @@
 import '#/imports'
+import '../../.dev/styled-system/styles.css'
 
 import { ReactNode, useState } from 'react'
+
+import { Box } from '../../.dev/styled-system/jsx'
 
 import { SXProvider } from './mitosis/design/SX'
 import { LayoutRoot } from './mitosis/universal/Layout'
@@ -19,7 +22,7 @@ export default class App {
                 initialPalette={palette}
             >
                 <LayoutRoot variant="landing" fullHeight="viewport">
-                    <Box sx="container mx-auto" bg="blue.500">
+                    <Box bg="blue.300" p="20" mx="auto" maxW="4xl" borderRadius="lg">
                         <Box>ARCH: {ARCH}</Box>
                         <Box>PLATFORM: {PLATFORM}</Box>
                         <Box>OS: {OS}</Box>
