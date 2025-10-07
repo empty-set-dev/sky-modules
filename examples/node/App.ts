6dd0aeba1511549f7beae143ac0c82305cc0c32e
Анна Севрюкова <a.sevrukova@getic.com>
2025-10-07 06:43:52 +0300

    Add features slice and update examples with new modules

--- examples/node/App.ts
+++ examples/node/App.ts
@@ -1,13 +1,6 @@
 import '#/imports'
 import Console from '@sky-modules/core/Console'
 
-@injectable()
-export default class App {
-    async create(): Promise<void> {
-        await main()
-    }
-}
-
-async function main(): Promise<void> {
+export default async function App(): Promise<void> {
     Console.success('Hello, world!')
 }
