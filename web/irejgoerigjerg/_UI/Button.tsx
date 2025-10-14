ada08afb7f5263e73af2ec1bf25f6b7db2d27e02
Anya Keller <a.sevrukova@getic.com>
2025-10-14 14:03:49 +0300

    Clean up web directory by removing temporary test files

--- web/irejgoerigjerg/_UI/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { Button as ReactNativeButton, ButtonProps } from 'react-native'
-
-export default function Button(props: ButtonProps): ReactNode {
-    const defaultProps: Partial<ButtonProps> = {
-        color: '#000000',
-    }
-
-    return <ReactNativeButton {...defaultProps} {...props} />
-}
