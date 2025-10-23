64d7caf67a33901470114f3ec4c3c0e4501914b3
Anya Keller <a.sevrukova@getic.com>
2025-10-23 19:42:26 +0300

    Fix namespace export handling in global file generation
    - Detect namespace exports (namespace Name ... export default Name)
    - For namespaces: only generate type declaration (no const)
    - Don't extract internal namespace members (accessible via Namespace.Member)
    - Don't pass namespace to globalify (it's a type, not a value)
    - Example: namespace Mitosis -> only type Mitosis in global scope

--- universal/Mitosis/Mitosis.global.ts
+++ universal/Mitosis/Mitosis.global.ts
@@ -3,11 +3,7 @@ import globalify from '@sky-modules/core/globalify'
 import Mitosis, * as imports from './Mitosis'
 
 declare global {
-    const Mitosis: typeof imports.default
     type Mitosis = typeof imports.default
-    type Node = imports.Node
-    type Children = imports.Children
-    type Ref = imports.Ref
 }
 
-globalify({ Mitosis, ...imports })
+globalify({ ...imports })
