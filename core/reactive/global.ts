0ab9e6c2f25e5d0c1c7276603c5387fd1a5d2a38
Anya Keller <a.sevrukova@getic.com>
2025-10-24 19:35:49 +0300

    Revert reactive module moves - keep files in define module

---
+++ core/reactive/global.ts
@@ -0,0 +1,7 @@
+// Auto-generated global imports
+
+import './observe.global'
+import './reaction.global'
+import './reactivePropertyDescriptors.global'
+import './save.global'
+import './share.global'
