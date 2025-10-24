6fa8a52848ea29cee1f58d23a3693e78eaa9a06c
Anya Keller <a.sevrukova@getic.com>
2025-10-24 19:35:49 +0300

    Revert reactive module moves - keep files in define module

---
+++ core/reactive/index.ts
@@ -0,0 +1,10 @@
+// Auto-generated index file
+
+export * from './observe'
+export { default as reaction } from './reaction'
+export * from './reaction'
+export { default as reactivePropertyDescriptors } from './reactivePropertyDescriptors'
+export * from './reactivePropertyDescriptors'
+export { default as save } from './save'
+export * from './save'
+export * from './share'
